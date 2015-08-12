cab740e6d961b4b545024e5f0a95fdd2be27d454
JW <walaro@kth.se>
2015-08-12 15:42:01 +0200

    Static file links

--- root/views.py
+++ root/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def index(request):
     if request.user.is_authenticated():
         username = request.user.username
-        return HttpResponse("You are logged in as %s, <a href=\"logout\">Logout</a>" % username)
+        return render(request, 'root.html')
     return render(request, 'login.html')
 
 # Create your views here.
