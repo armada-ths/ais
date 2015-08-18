e8f6f5d648a96430be12b90d7c3ba905e3724df8
JW <walaro@kth.se>
2015-08-18 12:10:53 +0200

    Added is_role group field

--- ais/settings.py
+++ ais/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cas',
+    'root',
 )
 
 MIDDLEWARE_CLASSES = (
--- root/models.py
+++ root/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import Group
 # Create your models here.
+
+Group.add_to_class('is_role', models.BooleanField(default=False))
+
