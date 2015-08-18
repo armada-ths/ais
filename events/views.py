aba44b3b59c85c0c8ceac3584d8358a98ec4e531
JW <walaro@kth.se>
2015-08-18 13:26:54 +0200

    Fixed event view to correct html file

--- events/views.py
+++ events/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def index(request):
     if request.user.is_authenticated():
         username = request.user.username
-        return render(request, 'root.html')
+        return render(request, 'events.html')
     return render(request, 'login.html')
 
 # Create your views here.
