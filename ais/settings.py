5e6e598a51dbac6ae53c05f03c64477a53ca2241
JW <walaro@kth.se>
2015-08-24 17:07:32 +0200

    Moved out DB name to secrets'

--- ais/settings.py
+++ ais/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'ais.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'ais',                      # Or path to database file if using sqlite3.
+            'NAME': DB_NAME,                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': DB_USERNAME,
             'PASSWORD': DB_PASSWORD,
