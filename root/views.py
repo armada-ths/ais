1e9e57f103c0bd65847d4192e1c34b4c424c7f8b
JW <walaro@kth.se>
2015-09-02 13:55:45 +0200

    Porting to Python3

--- root/views.py
+++ root/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from lib.banquet_placement import make_placement
 from threading import Thread
 
 def index(request):
     if request.user.is_authenticated():
         username = request.user.username
         return render(request, 'root.html')
-    t= Thread(target=make_placement)
-    t.start()
     return render(request, 'login.html')
 
 # Create your views here.
