45890629677704ed2bcb9c597c4fa684cef54109
JW <walaro@kth.se>
2015-09-21 15:30:25 +0200

    People list

--- people/views.py
+++ people/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-    
+from django.template.response import TemplateResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 def list_people(request):
     if request.user.is_authenticated():
         username = request.user.username
-        return render(request, 'people.html')
+        return TemplateResponse(request, 'people.html', {'users': User.objects.all()})
+        #return render(request, 'people.html')
     return render(request, 'login.html')
     
