910201d04478c8f258e5eb7d35046eee4324a9ea
JW <walaro@kth.se>
2015-09-21 23:48:22 +0200

    View single

--- people/views.py
+++ people/views.py
@@ -10,5 +10,5 @@ def list_people(request):
 
 @login_required(login_url='/login/')
 def view_person(request, pk):
-    return TemplateResponse(request, 'people_list.html', {'users': User.objects.all(id=pk)})
+    return TemplateResponse(request, 'people_list.html', {'users': User.objects.get(id=pk)})
     
