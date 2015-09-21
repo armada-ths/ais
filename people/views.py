acf07498626bc3e0428df6fe10deadf827efaa0b
JW <walaro@kth.se>
2015-09-21 15:39:30 +0200

    Login req dec added

--- people/views.py
+++ people/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 @login_required(login_url='/login/')
