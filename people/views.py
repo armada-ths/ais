9e527bc2ad7abb008f9ee54dbbcae94500cb233c
JW <walaro@kth.se>
2015-09-21 13:16:01 +0200

    Added fair to admin?

--- people/views.py
+++ people/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 def list_people(request):
     if request.user.is_authenticated():
         username = request.user.username
-        return render(request, 'root.html')
+        return render(request, 'people.html')
     return render(request, 'login.html')
     
