47c25128131d02d869212d05c61ac9ce78ce676e
JW <walaro@kth.se>
2015-09-21 23:46:12 +0200

    View single

--- people/urls.py
+++ people/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.list_people, name='list_people'),
+    url(r'^$', views.list_people, name='people'),
     url(r'^(?P<pk>\d+)$', views.view_person, name='view_person'),
 ]
