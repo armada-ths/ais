cb4116f96e7dfd8ba118869c59ecc6cdadf22f9a
JW <walaro@kth.se>
2015-09-21 13:36:57 +0200

    Added fair to admin?

--- people/urls.py
+++ people/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatters = [
-    url(r'^$', views.list_people, name="list_people"),
+    url(r'^$', views.list_people, name='list_people'),
 ]
