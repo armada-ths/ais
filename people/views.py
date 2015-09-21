274529f6c97ec181ae8e616878678050f5a061c1
JW <walaro@kth.se>
2015-09-21 23:53:06 +0200

    View single v4

--- people/views.py
+++ people/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from person.models import Person
 # Create your views here.
 
 @login_required(login_url='/login/')
