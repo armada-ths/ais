729d049c6d00064ccca5aaf5398f929532315add
ericmolin <ericmolin@mail.se>
2015-09-22 14:05:54 +0200

    Update and rename run_server.sh to run_server.sh.example

--- run_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-/bin/bash -c ". /home/eric/ais_venv/bin/activate; 
-python /home/eric/ais/manage.py makemigrations --settings local_settings;
-echo making migrations...;
-python /home/eric/ais/manage.py migrate --settings local_settings;
-echo migrating...;
-python /home/eric/ais/manage.py runserver 127.0.0.1:8080 --settings local_settings"
