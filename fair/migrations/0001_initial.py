c6ca9259fba1b2855b5090f791cd3e455156dd98
Eric Molin <ericmolin@mail.se>
2015-09-22 14:02:27 +0200

    fix companies

---
+++ fair/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Fair',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+        ),
+    ]
