f571e028d7bd867912c7a89084a63ef07e33fbe0
JW <walaro@kth.se>
2015-09-22 08:49:27 +0200

    Changed n to profile

--- people/admin.py
+++ people/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import People
+from .models import Profile
 
-admin.site.register(People)
+admin.site.register(Profile)
 
 # Register your models here.
