7c6c230945403deeba2a2834e90f8b0f6830fda6
sami purmonen <purmonen@kth.se>
2016-04-01 20:05:35 +0200

    Added recruitment periods to database

--- recruitment/models.py
+++ recruitment/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from fair.models import Fair
+# Model for company
+class RecruitmentPeriod(models.Model):
+    name = models.CharField(max_length=30)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    fair = models.ForeignKey('fair.Fair')
--- recruitment/views.py
+++ recruitment/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from models import RecruitmentPeriod
+
 # Create your views here.
 #list all companies
 def recruitment(request, template_name='recruitment/recruitment.html'):
+    recruitmentPeriods = RecruitmentPeriod.objects.all()
     data = {}
+    data['recruitment_periods'] = recruitmentPeriods
     return render(request, template_name, data)
