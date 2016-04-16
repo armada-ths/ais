8cb673049f250737d27ef71bf8fb9162b4aeedd5
walaro <walaro@kth.se>
2016-04-16 17:09:56 +0200

    Updated static files for django 1.9.5
    Please make sure that all instances of django are also updated

--- local_settings.py
+++ local_settings.py
@@ -20,7 +20,6 @@ SECRET_KEY = 'gdfjngj4n"#87234isd9543988au9u89af39")3'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -130,10 +129,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+ADMIN_MEDIA_PREFIX = '/static/admin/'
