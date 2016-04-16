03f65be1a75f757c094f20da2e283d28cf9578e1
walaro <walaro@kth.se>
2016-04-16 17:16:52 +0200

    Added ADMIN_MEDIA_PREFIX to production settings

--- ais/settings.py
+++ ais/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = 'static'
-
 STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 STATICFILES_DIRS = (
 )
