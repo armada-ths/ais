bdcc184ac31de900642e026b5011990ea6f4f7c7
JW <walaro@kth.se>
2016-05-28 01:02:55 +0200

    Added year field to fair

--- fair/models.py
+++ fair/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from datetime import date
 
 class Fair(models.Model):
-    name = models.CharField(max_length=100)
-    
+    name = models.CharField(max_length=100, default="Armada %d"%(date.today().year))
+    year = models.IntegerField(default=date.today().year) 
     def __str__(self):
         return '%s'%(self.name)
