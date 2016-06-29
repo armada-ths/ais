2c904e0823f07d68812855dcd685021779a8a6af
kennedal <kennedal@kth.se>
2016-06-29 18:52:51 +0200

    wrote basic model for news article

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=150)
+    html_text = models.CharField(max_length=5000)
+    publication_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
