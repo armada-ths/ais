ceb325153e980ddc96ed4effb3783ce6303edcce
kennedal <kennedal@kth.se>
2016-06-29 19:42:26 +0200

    improved structure of admin pages for news

--- news/admin.py
+++ news/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import NewsArticle
 
-admin.site.register(NewsArticle)
+class NewsArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Content", {"fields": ["title", "html_article_text"]}),
+        ("Publication time", {"fields": ["publication_date"]})
+    ]
+
+    # List view
+    list_display = ("title", "publication_date")
+
+admin.site.register(NewsArticle, NewsArticleAdmin)
