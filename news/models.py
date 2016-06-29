2c50634311768a8a05bf9c19b98481ed8658c074
kennedal <kennedal@kth.se>
2016-06-29 19:29:17 +0200

    updated news article model to use tinymce

--- news/models.py
+++ news/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 class NewsArticle(models.Model):
     title = models.CharField(max_length=150)
-    html_text = models.CharField(max_length=5000)
+    html_article_text = HTMLField(default="")
     publication_date = models.DateTimeField()
 
     def __str__(self):
