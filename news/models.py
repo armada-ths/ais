176d19caebb62761c322789407297c0bfc09814a
kennedal <kennedal@kth.se>
2016-07-15 12:44:16 +0200

    created a model manager for newsarticles to only get the articles that should be public

--- news/models.py
+++ news/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from tinymce.models import HTMLField
+from django.utils import timezone
+
+
+# Manager for getting only the articles that should be displayed
+class NewsArticleManager(models.Manager):
+    def get_queryset(self):
+        return super(NewsArticleManager, self).get_queryset().filter(publication_date__lte=timezone.now())
 
 class NewsArticle(models.Model):
     title = models.CharField(max_length=150)
@@ -8,3 +15,6 @@ class NewsArticle(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager() # keep the default manager
+    public_articles = NewsArticleManager() # add custom manager
