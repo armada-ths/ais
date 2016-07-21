113465f22bb7163f284e28fff09921ead8be84af
JW <walaro@kth.se>
2016-07-21 16:19:21 +0200

    Added editor to new with form/submit working

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.news_list, name='news'),
-    #url(r'^new$', views.news_article_create, name='news_article_new'),
+    url(r'^new$', views.news_article_create, name='news_article_new'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django import forms
+from django.forms import ModelForm
+from django.forms import DateInput
+from django.forms import DateTimeField
+import datetime
+
 from news.models import NewsArticle
 # Create your views here.
+
+class NewsArticleForm(ModelForm):
+    publication_date = DateTimeField(initial=datetime.datetime.now)
+    html_article_text = forms.CharField(widget=forms.Textarea)
+    class Meta:
+        model = NewsArticle
+        fields = '__all__'
+        widgets = {
+            'publication_date': DateInput(attrs={'class':'datepicker'}),
+        }
+
 
 def news_list(request, template_name='news/news_list.html'):
     news = NewsArticle.objects.all()
     data = {}
     data['object_list'] = news
     return render(request, template_name, data)
+
+def news_article_create(request, template_name='news/article_form.html'):
+    form = NewsArticleForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('news')
+    return render(request, template_name, {'form':form})
+
