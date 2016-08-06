119963df31ac5c9de32a4881d14440eabd2ac3e4
sami purmonen <purmonen@kth.se>
2016-08-06 18:29:31 +0200

    Updated vagrant provision file so it creates migrations

--- scripts/vagrant/provision.sh
+++ scripts/vagrant/provision.sh
@@ -9,6 +9,7 @@ virtualenv ais_venv
 source ais_venv/bin/activate
 pip3 install -r requirements.txt
 
+python manage.py makemigrations --settings local_settings
 python manage.py migrate --settings local_settings
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell --settings local_settings 
