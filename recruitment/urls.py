73ee20f33ee1412b8edce6678cccebdc40ed99dd
sami purmonen <purmonen@kth.se>
2016-08-06 17:49:53 +0200

    Removed imports so it works with Django 1.10

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.conf.urls import patterns
+
 from django.conf import settings
 from django.conf.urls.static import  static
 urlpatterns = [
