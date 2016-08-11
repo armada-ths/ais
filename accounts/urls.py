b1cf176e881835f124ab5f9d722914df9db219cc
sami purmonen <purmonen@kth.se>
2016-08-11 02:01:57 +0200

    Changed url format for login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,17 +3,20 @@ from . import views
 
 from django.conf import settings
 from django.conf.urls.static import  static
+
+from django.contrib.auth.views import login, logout
+
 urlpatterns = [
     #url(r'^login/$', views.login, name='login'),
     url(
         r'^login/$',
-        'django.contrib.auth.views.login',
+        login,
         name='login',
         kwargs={'template_name': 'accounts/login.html'}
     ),
     url(
         r'^logout/$',
-        'django.contrib.auth.views.logout',
+        logout,
         name='logout',
         kwargs={'next_page': '/'}
     ),
