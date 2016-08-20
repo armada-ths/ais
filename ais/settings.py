ed0332cb42f0a48dd2831cb5108c33f5878483d6
sami purmonen <purmonen@kth.se>
2016-08-21 00:47:24 +0200

    Removed CAS_REDIRECT_URL since it caused infinite loop

--- ais/settings.py
+++ ais/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 CAS_SERVER_URL = 'https://login.kth.se/'
 CAS_IGNORE_REFERER = True
-CAS_REDIRECT_URL = 'armada.nu/login'
+#CAS_REDIRECT_URL = 'armada.nu/login'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = ['.armada.nu']
