7db056a0d4c3d4b572203bcedb77e47ea969948a
sami purmonen <purmonen@kth.se>
2016-08-25 21:49:24 +0200

    Added field to user table in admin interface

--- root/admin.py
+++ root/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-#from fair import Fair
 
-#Create a fair will be done in Admin GUI
-#admin.site.register(Fair)
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_staff', 'last_login')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
