acba75f271e7a78740b614a3a7c5301f55208b80
sami purmonen <purmonen@kth.se>
2016-08-27 13:46:31 +0200

    Added tests for permissions

--- recruitment/tests.py
+++ recruitment/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import RecruitmentPeriod, RecruitmentApplication
+from .models import RecruitmentPeriod, RecruitmentApplication, Role, RoleApplication, AISPermission
 from fair.models import Fair
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -10,7 +10,7 @@ class RecruitmentTestCase(TestCase):
 
 
     def setUp(self):
-        fair = Fair.objects.create(name="Armada 2016")
+        fair = Fair.objects.create(name="Armada 2016", year=timezone.now().year)
 
         self.now = timezone.now()
         self.tomorrow = self.now + timezone.timedelta(days=1)
@@ -23,6 +23,21 @@ class RecruitmentTestCase(TestCase):
             interview_end_date=self.yesterday,
             fair=fair)
 
+        self.pg_role = Role.objects.create(name='PG')
+        self.system_developer = Role.objects.create(name='System Developer', parent_role=self.pg_role)
+        self.career_fair_leader = Role.objects.create(name='Career Fair Leader')
+
+        self.administer_roles_permission = AISPermission.objects.create(codename='administer_roles', name='Administer roles')
+        self.administer_recruitment_permission = AISPermission.objects.create(codename='administer_recruitment', name='Administer recruitment')
+
+        self.pg_role.permissions.add(self.administer_roles_permission)
+        self.system_developer.permissions.add(self.administer_recruitment_permission)
+        self.career_fair_leader.permissions.add(self.administer_recruitment_permission)
+
+
+
+        self.recruitment_period.recruitable_roles.add(self.system_developer, self.career_fair_leader)
+
         self.purmonen_user = User.objects.create(username='purmonen')
         self.bratteby_user = User.objects.create(username='bratteby')
 
@@ -31,10 +46,24 @@ class RecruitmentTestCase(TestCase):
             recruitment_period=self.recruitment_period
         )
 
+        RoleApplication.objects.create(
+            recruitment_application=self.recruitment_application,
+            role=self.system_developer,
+            order=2
+        )
+
+        RoleApplication.objects.create(
+            recruitment_application=self.recruitment_application,
+            role=self.career_fair_leader,
+            order=1
+        )
+
 
     def test_recruitment_period(self):
         self.assertNotEqual(self.recruitment_period.interview_questions, None)
         self.assertNotEqual(self.recruitment_period.application_questions, None)
+
+        self.assertEqual(len(self.recruitment_period.recruitable_roles.all()), 2)
 
         self.assertEqual(self.recruitment_period.is_past(), True)
         self.assertEqual(self.recruitment_period.is_future(), False)
@@ -59,4 +88,30 @@ class RecruitmentTestCase(TestCase):
 
         self.recruitment_application.interview_date = self.yesterday
         self.assertEqual(self.recruitment_application.state(), 'interview_done')
+
+        self.recruitment_application.delegated_role = self.system_developer
+        self.assertEqual(self.recruitment_application.state(), 'interview_done')
+
+        self.recruitment_application.status = 'accepted'
+        self.assertEqual(self.recruitment_application.state(), 'accepted')
+
+        self.recruitment_application.save()
+
+        self.assertTrue('administer_recruitment' in self.purmonen_user.ais_permissions())
+        self.assertTrue('administer_roles' in self.purmonen_user.ais_permissions())
+
+        self.recruitment_application.delegated_role = self.career_fair_leader
+        self.recruitment_application.save()
+
+        self.assertTrue('administer_recruitment' in self.purmonen_user.ais_permissions())
+        self.assertTrue('administer_roles' not in self.purmonen_user.ais_permissions())
+
+        self.recruitment_application.status = 'rejected'
+        self.recruitment_application.save()
+        self.assertEqual(self.recruitment_application.state(), 'rejected')
+
+
+        self.assertTrue('administer_recruitment' not in self.purmonen_user.ais_permissions())
+        self.assertTrue('administer_roles' not in self.purmonen_user.ais_permissions())
+
 
