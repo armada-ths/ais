195a95480f9b58f1954bb071714170bf3c85699a
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-08-31 21:22:31 +0200

    Fix typo in production urls

--- ais/production/urls.py
+++ ais/production/urls.py
@@ -2,6 +2,6 @@ from ais.common.urls import *
 from django.conf.urls import include, url
 from cas import views as cas_views
 
-urlpatterns = +=[
+urlpatterns +=[
     url(r'^login/', cas_views.login, name='login'),
 ]
