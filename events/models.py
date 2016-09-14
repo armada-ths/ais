c5122b3876f54b2b0afc50e9c1a1de6c61bc2430
JW <walaro@kth.se>
2016-09-15 00:53:22 +0200

    Altered event model and made migrations

--- events/models.py
+++ events/models.py
@@ -7,12 +7,12 @@ class Event(models.Model):
     name = models.CharField(max_length=75)
     event_start = models.DateTimeField()
     event_end = models.DateTimeField()
-    capacity = models.IntegerField(default=1, blank=True, null=True)
+    capacity = models.IntegerField(default=0, blank=True, null=True)
     description = models.TextField(blank=True)
     registration_open = models.DateTimeField()
     registration_last_day = models.DateTimeField()
-    registration_last_day_cancel = models.DateTimeField()
-    public_registration = models.BooleanField()
+    registration_last_day_cancel = models.DateTimeField(null=True)
+    public_registration = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.name
@@ -20,10 +20,11 @@ class Event(models.Model):
     def __str__(self):
         return '%s'%(self.name)
 
-# An event question belongs to a specific event
+# An EventQuestion belongs to a specific Event
 class EventQuestion(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    question_text = models.CharField(max_length=256)
+    question_text = models.TextField(blank=False) 
+    required = models.BooleanField(default=False)
 
     def __unicode__(self):
         return '%s'%(self.question_text)
@@ -52,19 +53,14 @@ class EventAttendence(models.Model):
     
 
 # An EventAnswer is the answer to a specific EventQuestion for a specific User
-# This should be inlined in the admin interface
 class EventAnswer(models.Model):
     question = models.ForeignKey(EventQuestion, on_delete=models.CASCADE)
     attendence = models.ForeignKey(EventAttendence, on_delete=models.CASCADE)
-    answer = models.CharField(max_length=256)
+    answer = models.TextField(blank=True)
 
     def __unicode__(self):
         return '%s'%(self.question.question_text)
     
     def __str__(self):
         return '%s'%(self.question.question_text)
-
-# Function in model, explore where used and then remove
-def get_absolute_url(self):
-    return reverse('event_edit', kwargs={'pk': self.pk})
 
