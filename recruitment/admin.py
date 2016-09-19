e36d8ef4b8c02c2a2262f52c6152022df2433c55
Sami Purmonen <sami.purmonen@king.com>
2016-09-19 22:57:40 +0200

    Added possibility to modify custom fields, arguments and answers through the admin interface

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,31 +1,32 @@
 from django.contrib import admin
-from .models import RecruitmentPeriod, RecruitmentApplication, RoleApplication, CustomField, CustomFieldAnswer, CustomFieldArgument
+from .models import *
 
 
 class CustomFieldInline(admin.TabularInline):
     model = CustomField
 
+
 class CustomFieldAnswerInline(admin.TabularInline):
     model = CustomFieldAnswer
+
 
 class CustomFieldArgumentInline(admin.TabularInline):
     model = CustomFieldArgument
 
+
 class CustomFieldAdmin(admin.ModelAdmin):
     inlines = [CustomFieldArgumentInline]
 
-class RecruitmentPeriodAdmin(admin.ModelAdmin):
-    inlines = []
 
 class RoleApplicationInline(admin.TabularInline):
     model = RoleApplication
 
+
 class RecruitmentApplicationAdmin(admin.ModelAdmin):
     inlines = [RoleApplicationInline]
 
-admin.site.register(RecruitmentPeriod, RecruitmentPeriodAdmin)
+admin.site.register(RecruitmentPeriod)
 admin.site.register(RecruitmentApplication, RecruitmentApplicationAdmin)
 admin.site.register(CustomField, CustomFieldAdmin)
-
-#admin.site.register(ExtraField)
-#admin.site.register(CustomField)
+admin.site.register(CustomFieldAnswer)
+admin.site.register(CustomFieldArgument)
