bb6053838e0e445eb845661067de96829bed2d00
JW <walaro@kth.se>
2016-09-20 22:41:24 +0200

    Added orders and products

--- companies/models.py
+++ companies/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from fair.models import Fair
+
 
 # Model for company
 class Company(models.Model):
     class Meta:
         verbose_name_plural = 'Companies'
-    
+
     name = models.CharField(max_length=30)
     organisation_number = models.CharField(max_length=30)
     website = models.CharField(max_length=50)
@@ -13,15 +13,15 @@ class Company(models.Model):
 
     # Might be more elegant to split to separate model
     invoice_address_street = models.CharField(max_length=64, blank=True)
-    invoice_address_zip_code = models.CharField(max_length=8, blank=True) 
-    invoice_address_city = models.CharField(max_length=32, blank=True) 
-    invoice_address_country = models.CharField(max_length=32, blank=True) 
+    invoice_address_zip_code = models.CharField(max_length=8, blank=True)
+    invoice_address_city = models.CharField(max_length=32, blank=True)
+    invoice_address_country = models.CharField(max_length=32, blank=True)
 
     delivery_address_street = models.CharField(max_length=64, blank=True)
-    delivery_address_zip_code = models.CharField(max_length=8, blank=True) 
-    delivery_address_city = models.CharField(max_length=32, blank=True) 
-    delivery_address_country = models.CharField(max_length=32, blank=True) 
-    
+    delivery_address_zip_code = models.CharField(max_length=8, blank=True)
+    delivery_address_city = models.CharField(max_length=32, blank=True)
+    delivery_address_country = models.CharField(max_length=32, blank=True)
+
     def __str__(self):
         return self.name
 
@@ -31,22 +31,14 @@ class CompanyContact(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    title = models.CharField(max_length=50) #work title, such as CEO or HR.
+    title = models.CharField(max_length=50)  # work title, such as CEO or HR.
     cell_phone = models.CharField(max_length=50)
     work_phone = models.CharField(max_length=50)
-    active = models.BooleanField(default=True) #if the contact is active
+    active = models.BooleanField(default=True)  # if the contact is active
 
     def __str__(self):
         return self.name
 
-
-# An Exhibitor is a 'Company' perticipating in a specific 'Fair'
-class Exhibitor(models.Model):
-    company = models.ForeignKey(Company)
-    fair = models.ForeignKey('fair.Fair')
-
-
 # TODO some model to be able to add a company to sales/sponsorship
-
 
 # TODO some model to be able to comment on a sale/sponsorship
---
+++ orders/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from fair.models import Fair
+from companies.models import Exhibitor
+
+# A 'Product' is a purchasable item that belongs to a 'Fair'
+class Product(models.Model):
+    fair = models.ForeignKey('fair.Fair')
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256, blank=True) 
+    # Chart of accounts number (sv. kontonummer), used for accounting
+    coa_number = models.PositiveSmallIntegerField()
+    price = models.IntegerField()
+
+# An 'Exhibitor' places an 'Order' for a 'Product'
+class Order(models.Model):
+    exhibitor = models.ForeignKey('companies.Exhibitor', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product)
+    amount = models.PositiveSmallIntegerField()
