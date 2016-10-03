b44b42a7d8e811e8672ae138529c62bbaf9207d0
JW <walaro@kth.se>
2016-10-03 11:16:02 +0200

    Added mail on creation of new attendence

--- events/views.py
+++ events/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 from events.forms import AttendenceForm
 from events.models import EventQuestion, Event, EventAnswer, EventAttendence
 from events.templatetags.event_extras import user_attending_event
+from django.core.mail import send_mail
+
+def send_mail_on_submission(user, event):
+    if user.email != "":
+        send_mail(
+            event.submission_mail_subject,
+            event.submission_mail_body,
+            'system@armada.nu',
+            [user.email],
+            fail_silently=False,
+        )
 
 def event_attend_form(request, pk, template_name='events/event_attend.html'):
     event = get_object_or_404(Event, pk=pk)
@@ -12,8 +23,11 @@ def event_attend_form(request, pk, template_name='events/event_attend.html'):
     if form.is_valid():
         if not ea:
             ea = EventAttendence.objects.create(user=request.user, event=event)
+            if event.send_submission_mail:
+                send_mail_on_submission(request.user, event)
         for (question, id, answer) in form.get_answers():
             EventAnswer.objects.update_or_create(question_id=id, attendence=ea, defaults={'answer':answer})
+        
         return redirect('event_list')
 
     return render(request, template_name, {"event":event, "form":form})
