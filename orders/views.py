23ba2785a0fdf60158782da4cad997631103ea3b
Sami Purmonen <sami.purmonen@king.com>
2016-10-15 23:10:10 +0200

    Added revenue and sorting to products table

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,8 @@ from .models import Product
 
 # Create your views here.
 def products(request, template_name='orders/products.html'):
+	products = Product.objects.filter(fair=Fair.objects.get(name='Armada 2016'))
 	return render(request, template_name, {
-		'products': Product.objects.filter(fair=Fair.objects.get(name='Armada 2016'))
+		'products': products,
+		'total_revenue': sum([product.revenue() for product in products])
 	})
