1696041b2ef072386bc0b4380d5023e8dfc28c59
JW <walaro@kth.se>
2016-10-18 22:10:35 +0200

    Added event name to search

--- events/admin.py
+++ events/admin.py
@@ -69,7 +69,8 @@ class AnswerInline(admin.StackedInline):
 
 class EventAttendenceAdmin(admin.ModelAdmin):
     inlines = [AnswerInline]
-    search_fields = ['user__first_name', 'user__last_name', 'user__email']
+    search_fields = ['user__first_name', 'user__last_name', 'user__email', 
+        'event__name']
     list_filter = ('event',)
 
 admin.site.register(Event, EventAdmin)
