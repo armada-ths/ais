5ed64b4257d0d1d5758eb483415a3f4de65e4695
JW <walaro@kth.se>
2016-10-18 21:07:36 +0200

    Added CSV headers and user information to each export

--- events/admin.py
+++ events/admin.py
@@ -17,19 +17,32 @@ def export_as_csv(modeladmin, request, queryset):
 
     # We have already checked that it is a single event    
     event_id = queryset.first().id
+    csv_headers = ['First name (user)', 'Last name (user)', 'Email (user)']
     question_list = []
     for question in EventQuestion.objects.filter(event__id = event_id):
         question_list.append(question.id)
+        csv_headers.append(question.question_text)
 
     writer = csv.writer(response, quoting=csv.QUOTE_ALL)
+    writer.writerow(csv_headers)
     for attendence in EventAttendence.objects.filter(event__id = event_id):
+        user = attendence.user
+        if user == None:
+            user_information = [None, None, None]
+        else:
+            user_information = [
+                attendence.user.first_name,
+                attendence.user.last_name,
+                attendence.user.email,
+            ]
         answers = []
+        # This requires an answer to all questions, atleast a null answer.
         for question in question_list:
             answers.append(EventAnswer.objects
                            .filter(question__id =  question)
                            .filter(attendence__id = attendence.id)
                            .first().answer)
-        writer.writerow(answers)
+        writer.writerow(user_information + answers)
     return response
 
 class QuestionInline(admin.StackedInline):
