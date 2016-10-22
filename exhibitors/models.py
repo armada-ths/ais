828842960344a5399616540eefe3467f42cc4fd8
Sami Purmonen <sami.purmonen@king.com>
2016-10-22 18:32:20 +0200

    Show exhibitor banquet attendents using panels instead of a table since there's not enough room in the table

--- exhibitors/models.py
+++ exhibitors/models.py
@@ -109,6 +109,9 @@ class BanquetteAttendant(models.Model):
     wants_lactose_free_food = models.BooleanField(default=False)
     wants_gluten_free_food = models.BooleanField(default=False)
 
+    def __str__(self):
+        return '%s %s - %s' % (self.first_name, self.last_name, self.exhibitor)
+
 # Work field that an exhibitor operates in
 class WorkField(models.Model):
     name = models.CharField(max_length=64)
