8ca34cc95c4611cafc11ac3fe46b40f67b3e96d9
Andreas Pålsson <apals@kth.se>
2016-10-23 18:07:27 +0200

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -14,7 +14,7 @@ def tags_mappings(items):
 
 
 def absolute_url(request, path):
-    protocol = 'https://' if request.is_secure else 'http://'
+    protocol = 'https://' if request.is_secure() else 'http://'
     url = request.META['HTTP_HOST']
     return '{}{}{}'.format(protocol, url, path)
 
