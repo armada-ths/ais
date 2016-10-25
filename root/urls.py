cf53fae17e9dfa64a314ee22551bba544767930b
Sami Purmonen <sami.purmonen@king.com>
2016-10-25 22:44:52 +0200

    Fixed correct banquet urls

--- root/urls.py
+++ root/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^banquette_signup$', views.banquette_signup, name='banquette_signup'),
-    url(r'^banquette_signup_delete$', views.banquette_signup_delete, name='banquette_signup_delete'),
+    url(r'^banquet_signup$', views.banquette_signup, name='banquette_signup'),
+    url(r'^banquet_signup_delete$', views.banquette_signup_delete, name='banquette_signup_delete'),
 ]
