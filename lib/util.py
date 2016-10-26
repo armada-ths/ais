1c948a577f8654452547b7b5b911520e3411a727
Tiseno <math.lindgren@gmail.com>
2016-10-26 22:33:29 +0200

    Check if user is eligible to see/attend event

--- lib/util.py
+++ lib/util.py
@@ -2,6 +2,12 @@ from django.utils import timezone
 import time
 
 
+def has_common_element(list1, list2):
+    if any(e in list2 for e in list1):
+        return True
+    return False
+
+
 def before(time):
     now = timezone.now()
     return now < time
