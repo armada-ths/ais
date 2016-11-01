1e31018de75c99c622d74629c6fb59de09e69e56
Michel Tabari <tabari@kth.se>
2016-11-01 09:29:07 +0100

    Add initial logging setup (#141)

--- ais/production/settings.py
+++ ais/production/settings.py
@@ -5,6 +5,7 @@ and also configures AIS to talk to external services (e.g. CAS).
 """
 
 import os
+from os import environ as env
 from ais.common.settings import *
 from ais.secrets import *
 
@@ -17,7 +18,7 @@ ALLOWED_HOSTS = ['.armada.nu', 'localhost']
 ROOT_URLCONF = 'ais.production.urls'
 
 # Use KTH CAS for authentication
-INSTALLED_APPS += ('cas',)
+INSTALLED_APPS += ('cas', 'raven.contrib.django.raven_compat',)
 AUTHENTICATION_BACKENDS += ('cas.backends.CASBackend',)
 CAS_SERVER_URL = 'https://login.kth.se/'
 CAS_AUTO_CREATE_USER = False
@@ -32,6 +33,56 @@ DATABASES = {
             'PASSWORD': DB_PASSWORD,
             'HOST': DB_HOST,
     }
+}
+
+# SENTRY
+RAVEN_CONFIG = {
+    'dsn': 'https://%s:%s@sentry.io/%s' % (env['SENTRY_USERNAME'], env['SENTRY_PASSWORD'], env['SENTRY_APPID']),
+    'processors': ('raven.processors.Processor',)
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'root': {
+            'level': 'WARNING',
+            'handlers': ['sentry'],
+        },
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
 }
 
 # The system sends out system-related emails to these addresses.
