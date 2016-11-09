356e94ae8360ac957449e423ef9eed4ff81d40b2
Michel Tabari <tabari@kth.se>
2016-11-09 21:34:35 +0100

    [WIP] Location dude (#180)
    * Location stuff
    
    * Add room to serializer for catalog info
    
    * Fix missing room

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Building, Location, Room
 
-# Register your models here.
+admin.site.register(Building)
+admin.site.register(Location)
+admin.site.register(Room)
--- locations/views.py
+++ locations/views.py
@@ -1,32 +1,32 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.forms import ModelForm
-from locations.models import Location
-from locations.forms import LocationForm
+# from django.shortcuts import render, redirect, get_object_or_404
+# from django.forms import ModelForm
+# from locations.models import Location
+# from locations.forms import LocationForm
 
-def location_list(request, template_name='locations/location_list.html'):
-    locations = Location.objects.all()
-    data = {}
-    data['object_list'] = locations
-    return render(request, template_name, data)
-
-def location_create(request, template_name='locations/location_form.html'):
-    form = LocationForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('location_form')
-    return render(request, template_name, {'form':form})
-
-def location_update(request, pk, template_name='locations/location_form.html'):
-    location = get_object_or_404(Location, pk=pk)
-    form = LocationForm(request.POST or None, instance=location)
-    if form.is_valid():
-        form.save()
-        return redirect('location_list')
-    return render(request, template_name, {'form':form})
-
-def location_delete(request, pk, template_name='locations/location_confirm_delete.html'):
-    location = get_object_or_404(Location, pk=pk)
-    if request.method=='POST':
-        location.delete()
-        return redirect('location_list')
-    return render(request, template_name, {'object':location})
+# def location_list(request, template_name='locations/location_list.html'):
+#     locations = Location.objects.all()
+#     data = {}
+#     data['object_list'] = locations
+#     return render(request, template_name, data)
+#
+# def location_create(request, template_name='locations/location_form.html'):
+#     form = LocationForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('location_form')
+#     return render(request, template_name, {'form':form})
+#
+# def location_update(request, pk, template_name='locations/location_form.html'):
+#     location = get_object_or_404(Location, pk=pk)
+#     form = LocationForm(request.POST or None, instance=location)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('location_list')
+#     return render(request, template_name, {'form':form})
+#
+# def location_delete(request, pk, template_name='locations/location_confirm_delete.html'):
+#     location = get_object_or_404(Location, pk=pk)
+#     if request.method=='POST':
+#         location.delete()
+#         return redirect('location_list')
+#     return render(request, template_name, {'object':location})
