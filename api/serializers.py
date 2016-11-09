57cfeca0656c3e636bf4348660b1611c2ee8c17d
Michel Tabari <tabari@kth.se>
2016-11-09 14:58:55 +0100

    Add location map to CatalogInfo (#177)

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,8 @@
 from collections import OrderedDict
 from lib.util import unix_time
 
-
 MISSING_IMAGE = '/static/missing.png'
+MISSING_MAP = '/static/nymble_2048.png'
 
 
 def tags_mappings(items):
@@ -23,7 +23,7 @@ def image_url_or_missing(request, image, missing=MISSING_IMAGE):
     if image:
         return absolute_url(request, image.url)
     else:
-        return absolute_url(request, MISSING_IMAGE)
+        return absolute_url(request, missing)
 
 
 def obj_name(obj):
@@ -32,7 +32,7 @@ def obj_name(obj):
     return OrderedDict([
         ('id', obj.pk),
         ('name', obj.name)
-        ])
+    ])
 
 
 def names(objects):
@@ -42,50 +42,51 @@ def names(objects):
 def exhibitor(request, exhibitor):
     tags = tags_mappings(exhibitor.tags.all())
     return OrderedDict([
-        ('id', exhibitor.pk),
-        ('name', exhibitor.display_name),
-        ('slug', exhibitor.slug),
-        ('short_description', exhibitor.short_description),
-        ('description', exhibitor.description),
-        ('employees_sweden', exhibitor.employees_sweden),
-        ('employees_world', exhibitor.employees_world),
-        ('countries', exhibitor.countries),
-        ('website_url', exhibitor.website_url),
-        ('facebook_url', exhibitor.facebook_url),
-        ('twitter_url', exhibitor.twitter_url),
-        ('linkedin_url', exhibitor.linkedin_url),
-        ('logo_url', image_url_or_missing(request, exhibitor.logo)),
-        ('logo_small_url',
-            image_url_or_missing(request, exhibitor.logo_small)),
-        ('ad_url', image_url_or_missing(request, exhibitor.ad)),
-        ('programs', names(exhibitor.programs)),
-        ('main_work_field', obj_name(exhibitor.main_work_field)),
-        ('work_fields', names(exhibitor.work_fields)),
-        ('job_types', names(exhibitor.job_types)),
-        ('continents', names(exhibitor.continents)),
-        ('values', names(exhibitor.values)),
-    ] + tags)
+                           ('id', exhibitor.pk),
+                           ('name', exhibitor.display_name),
+                           ('slug', exhibitor.slug),
+                           ('short_description', exhibitor.short_description),
+                           ('description', exhibitor.description),
+                           ('employees_sweden', exhibitor.employees_sweden),
+                           ('employees_world', exhibitor.employees_world),
+                           ('countries', exhibitor.countries),
+                           ('website_url', exhibitor.website_url),
+                           ('facebook_url', exhibitor.facebook_url),
+                           ('twitter_url', exhibitor.twitter_url),
+                           ('linkedin_url', exhibitor.linkedin_url),
+                           ('logo_url', image_url_or_missing(request, exhibitor.logo)),
+                           ('logo_small_url',
+                            image_url_or_missing(request, exhibitor.logo_small)),
+                           ('ad_url', image_url_or_missing(request, exhibitor.ad)),
+                           ('map_location_url', image_url_or_missing(request, exhibitor.location_at_fair, MISSING_MAP)),
+                           ('programs', names(exhibitor.programs)),
+                           ('main_work_field', obj_name(exhibitor.main_work_field)),
+                           ('work_fields', names(exhibitor.work_fields)),
+                           ('job_types', names(exhibitor.job_types)),
+                           ('continents', names(exhibitor.continents)),
+                           ('values', names(exhibitor.values)),
+                       ] + tags)
 
 
 def event(request, event):
     tags = tags_mappings(event.tags.all())
     return OrderedDict([
-        ('id', event.pk),
-        ('name', event.name),
-        ('image_url', image_url_or_missing(request, event.image)),
-        ('location', event.location),
-        ('description_short', event.description_short),
-        ('description', event.description),
-        ('signup_link',
-            absolute_url(request, '/events/'+str(event.pk)+'/signup')),
-        ('event_start', unix_time(event.event_start)),
-        ('event_end', unix_time(event.event_end)),
-        ('registration_required', event.registration_required),
-        ('registration_start', unix_time(event.registration_start)),
-        ('registration_end', unix_time(event.registration_end)),
-        ('registration_last_day_cancel',
-            unix_time(event.registration_last_day_cancel)),
-    ] + tags)
+                           ('id', event.pk),
+                           ('name', event.name),
+                           ('image_url', image_url_or_missing(request, event.image)),
+                           ('location', event.location),
+                           ('description_short', event.description_short),
+                           ('description', event.description),
+                           ('signup_link',
+                            absolute_url(request, '/events/' + str(event.pk) + '/signup')),
+                           ('event_start', unix_time(event.event_start)),
+                           ('event_end', unix_time(event.event_end)),
+                           ('registration_required', event.registration_required),
+                           ('registration_start', unix_time(event.registration_start)),
+                           ('registration_end', unix_time(event.registration_end)),
+                           ('registration_last_day_cancel',
+                            unix_time(event.registration_last_day_cancel)),
+                       ] + tags)
 
 
 def newsarticle(request, newsarticle):
@@ -124,6 +125,7 @@ def organization_group(request, group):
         ('people', people),
     ])
 
+
 def banquet_placement(request, attendence, index):
     return OrderedDict([
         ('id', attendence.pk),
@@ -131,7 +133,7 @@ def banquet_placement(request, attendence, index):
         ('last_name', attendence.last_name),
         ('linkedin_url', attendence.linkedin_url),
         # Table and seat are mocked, since fields are not available
-        ('table', str(int(index/8))),
-        ('seat', str(index%8)),
+        ('table', str(int(index / 8))),
+        ('seat', str(index % 8)),
         ('job_title', attendence.job_title)
     ])
--- exhibitors/models.py
+++ exhibitors/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from recruitment.models import RecruitmentApplication
 
+
 class Location(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
-
 
 
 # A company (or organisation) participating in a fair
@@ -37,7 +37,6 @@ class Exhibitor(models.Model):
     heavy_duty_electric_equipment = models.CharField(max_length=500, blank=True)
     other_information_about_the_stand = models.CharField(max_length=500, blank=True)
 
-
     # Invoice
     invoice_reference = models.CharField(max_length=200, blank=True)
     invoice_reference_phone_number = models.CharField(max_length=200, blank=True)
@@ -62,12 +61,15 @@ class Exhibitor(models.Model):
 
     # Transport from fair
     transport_from_fair_types = [
-        ('third_party_builders_transport', 'We use a third-party to build our stand who will transport our goods from the fair'),
+        ('third_party_builders_transport',
+         'We use a third-party to build our stand who will transport our goods from the fair'),
         ('armada_transport', 'We use Armada Transport'),
-        ('self_transport', 'We will arrange our own transportation immediately after the fair (note that there is limited access for larger transportation services as the fair closes and it may take some time before your equipment can be picked up)'),
+        ('self_transport',
+         'We will arrange our own transportation immediately after the fair (note that there is limited access for larger transportation services as the fair closes and it may take some time before your equipment can be picked up)'),
     ]
 
-    transport_from_fair_type = models.CharField(choices=transport_from_fair_types, null=True, blank=True, max_length=300)
+    transport_from_fair_type = models.CharField(choices=transport_from_fair_types, null=True, blank=True,
+                                                max_length=300)
     number_of_packages_from_fair = models.IntegerField(default=0)
     number_of_pallets_from_fair = models.IntegerField(default=0)
 
@@ -75,7 +77,6 @@ class Exhibitor(models.Model):
     transport_from_fair_zip_code = models.CharField(max_length=100, blank=True)
     transport_from_fair_recipient_name = models.CharField(max_length=200, blank=True)
     transport_from_fair_recipient_phone_number = models.CharField(max_length=200, blank=True)
-
 
     # Marketing
     wants_information_about_events = models.BooleanField()
@@ -86,15 +87,17 @@ class Exhibitor(models.Model):
         return sum([order.price() for order in self.order_set.all()])
 
     def superiors(self):
-        accepted_applications = [RecruitmentApplication.objects.filter(status='accepted', user=host).first() for host in self.hosts.all()]
+        accepted_applications = [RecruitmentApplication.objects.filter(status='accepted', user=host).first() for host in
+                                 self.hosts.all()]
         return [application.superior_user for application in accepted_applications if application.superior_user]
 
     def __str__(self):
         return '%s at %s' % (self.company.name, self.fair.name)
 
+
 class BanquetteAttendant(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True) # Null for exhibitor representants
-    exhibitor = models.ForeignKey(Exhibitor, null=True, blank=True) # Null for non-exhibitor representants
+    user = models.ForeignKey(User, null=True, blank=True)  # Null for exhibitor representants
+    exhibitor = models.ForeignKey(Exhibitor, null=True, blank=True)  # Null for non-exhibitor representants
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
@@ -123,6 +126,7 @@ class BanquetteAttendant(models.Model):
 
     def __str__(self):
         return '%s %s - %s' % (self.first_name, self.last_name, self.exhibitor)
+
 
 # Work field that an exhibitor operates in
 class WorkField(models.Model):
@@ -159,9 +163,9 @@ class Value(models.Model):
 # Info about an exhibitor to be displayed in apps and on website
 class CatalogInfo(models.Model):
     exhibitor = models.OneToOneField(
-            Exhibitor,
-            on_delete=models.CASCADE,
-            )
+        Exhibitor,
+        on_delete=models.CASCADE,
+    )
     display_name = models.CharField(max_length=200)
     slug = models.SlugField(db_index=False, blank=True)
     short_description = models.CharField(max_length=200, blank=True)
@@ -179,25 +183,35 @@ class CatalogInfo(models.Model):
     # image and should be the only image uploaded,
     # the others are auto generated
     logo_original = models.ImageField(
-            upload_to=UploadToDirUUID('exhibitors', 'logo_original'),
-            blank=True,
-            )
+        upload_to=UploadToDirUUID('exhibitors', 'logo_original'),
+        blank=True,
+    )
     logo_small = models.ImageField(
-            upload_to=UploadToDir('exhibitors', 'logo_small'), blank=True)
+        upload_to=UploadToDir('exhibitors', 'logo_small'), blank=True)
     logo = models.ImageField(
-            upload_to=UploadToDir('exhibitors', 'logo'), blank=True)
+        upload_to=UploadToDir('exhibitors', 'logo'), blank=True)
 
     ad_original = models.ImageField(
-            upload_to=UploadToDirUUID('exhibitors', 'ad_original'), blank=True)
+        upload_to=UploadToDirUUID('exhibitors', 'ad_original'), blank=True)
     ad = models.ImageField(
-            upload_to=UploadToDir('exhibitors', 'ad'), blank=True)
+        upload_to=UploadToDir('exhibitors', 'ad'), blank=True)
+
+    location_at_fair_original = models.ImageField(
+        upload_to=UploadToDirUUID('exhibitors', 'location_at_fair_original'),
+        blank=True
+    )
+
+    location_at_fair = models.ImageField(
+        upload_to=UploadToDir('exhibitors', 'location_at_fair'),
+        blank=True
+    )
 
     # ManyToMany relationships
     programs = models.ManyToManyField('people.Programme', blank=True)
     main_work_field = models.ForeignKey(
-            WorkField, blank=True, null=True, related_name='+')
+        WorkField, blank=True, null=True, related_name='+')
     work_fields = models.ManyToManyField(
-            WorkField, blank=True, related_name='+')
+        WorkField, blank=True, related_name='+')
     job_types = models.ManyToManyField(JobType, blank=True)
     continents = models.ManyToManyField(Continent, blank=True)
     values = models.ManyToManyField(Value, blank=True)
@@ -221,4 +235,7 @@ class CatalogInfo(models.Model):
         self.ad = update_image_field(
             self.ad_original,
             self.ad, 640, 480, 'jpg')
+        self.location_at_fair = update_image_field(
+            self.location_at_fair_original,
+            self.location_at_fair, 408, 408, 'png')
         super(CatalogInfo, self).save(*args, **kwargs)
