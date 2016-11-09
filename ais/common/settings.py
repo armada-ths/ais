433f2be7d0c11bd65e4d49e3dd03a2c81eb01292
Michel Tabari <tabari@kth.se>
2016-11-09 19:44:03 +0100

    Gzip Etags Caching (#179)

--- ais/common/settings.py
+++ ais/common/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = (
     'django.contrib.humanize'
 )
 
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
