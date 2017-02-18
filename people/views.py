ecced92ef42e17d7084703acc1bcda83b50a4750
Sami Purmonen <sami.purmonen@king.com>
2017-02-18 22:10:02 +0100

    Updated people for Armada 2017

--- people/views.py
+++ people/views.py
@@ -11,15 +11,17 @@ from recruitment.views import set_image_key_from_request
 
 from django.forms import ModelForm
 from django.contrib.auth.decorators import permission_required
-
+from fair.models import Fair
 
 @permission_required('people.view_people', raise_exception=True)
-def list_people(request):
+def list_people(request, year):
+    fair = get_object_or_404(Fair, year=year)
     users = User.objects.filter(is_superuser=False)
-    return TemplateResponse(request, 'people_list.html', {'users': users})
+    return TemplateResponse(request, 'people_list.html', {'users': users, 'fair': fair})
 
 
-def view_person(request, pk):
+def view_person(request, year, pk):
+    fair = get_object_or_404(Fair, year=year)
     user = get_object_or_404(User, pk=pk)
 
     if (request.user == user) or request.user.has_perm('people.view_people'):
@@ -30,7 +32,8 @@ def view_person(request, pk):
         application = RecruitmentApplication.objects.filter(user=user, status='accepted').first()
         return TemplateResponse(request, 'view_person.html', {
             'person': profile,
-            'role': application.delegated_role if application else ""
+            'role': application.delegated_role if application else "",
+            'fair': fair,
         })
     else:
         raise PermissionDenied
@@ -50,7 +53,8 @@ class ProfileForm(ModelForm):
         labels= {'birth_date': 'Birth date (format: 2016-12-24)'}
 
 
-def edit_person(request, pk):
+def edit_person(request, year, pk):
+    fair = get_object_or_404(Fair, year=year)
     user = get_object_or_404(User, pk=pk)
     if not request.user == user:
         raise PermissionDenied
@@ -64,12 +68,13 @@ def edit_person(request, pk):
     if form.is_valid():
         form.save()
         set_image_key_from_request(request, profile, 'image', 'profile')
-        return redirect('view_person', user.pk)
+        return redirect('view_person', fair.year, user.pk)
 
     application = RecruitmentApplication.objects.filter(user=user, status='accepted').first()
     return TemplateResponse(request, 'edit_person.html', {
         'person': profile,
         'role': application.delegated_role if application else "",
-        'form': form
+        'form': form,
+        'fair': fair,
     })
 
