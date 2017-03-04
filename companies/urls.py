732387bc831696cbd550a5f4a8c92e9ff7fb01da
Marcus Larson <marcus@Marcuss-MacBook-Pro-2.local>
2017-03-04 18:07:28 +0100

    Sales can now create user account for company contact

--- companies/urls.py
+++ companies/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     #CRUD Company /company, /company/new, /company/edit/pk, /company/delete/pk
-#    url(r'^$', views.companies_list, name='companies_list'),
-#    url(r'^(?P<pk>\d+)$', views.list_company, name='company'),
-#    url(r'^new$', views.company_create, name='company_new'),
-#    url(r'^edit/(?P<pk>\d+)$', views.company_update, name='company_edit'),
-#    url(r'^delete/(?P<pk>\d+)$', views.company_delete, name='company_delete'),
+    url(r'^$', views.companies_list, name='companies_list'),
+    url(r'^(?P<pk>\d+)$', views.list_company, name='company'),
+    url(r'^new$', views.company_create, name='company_new'),
+    url(r'^edit/(?P<pk>\d+)$', views.company_update, name='company_edit'),
+    url(r'^delete/(?P<pk>\d+)$', views.company_delete, name='company_delete'),
     #CRUD Company contact person
-#    url(r'^(?p<pk>\d+)/new$', views.contact_create, name='contact_new'),
-    url(r'^contact/(?P<contact_pk>\d+)/toggle_active$', views.contact_state_toggle, 
+    url(r'^(?P<pk>\d+)/new/', views.contact_create, name='contact_new'),
+    url(r'^contact/(?P<contact_pk>\d+)/toggle_active/', views.contact_state_toggle, 
         name='contact_state_toggle'),
 
 ]
--- companies/views.py
+++ companies/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from companies.models import Company, Contact
+from fair.models import Fair
+
 
 ###COMPANY###
 class CompanyForm(ModelForm):
@@ -9,66 +13,94 @@ class CompanyForm(ModelForm):
         model = Company
         fields = '__all__'
 
+def current_fair():
+    return get_object_or_404(Fair, year=2017)
+
 #list all companies
 def companies_list(request, template_name='companies/companies_list.html'):
+    fair = current_fair()
     companies = Company.objects.all()
-    data = {}
-    data['object_list'] = companies
-    return render(request, template_name, data)
+    return render(request, template_name, dict(object_list=companies, fair=fair))
 
 #list one company
 def list_company(request, pk, template_name='companies/list_company.html'):
+    fair = current_fair()
     company = get_object_or_404(Company, pk=pk)
-    return render(request, template_name, {'company':company})
+    return render(request, template_name, {'company':company, 'fair':fair})
 
 #crate a company
 def company_create(request, template_name='companies/company_form.html'):
+    fair = current_fair()
     form = CompanyForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('companies_list')
-    return render(request, template_name, {'form':form})
+    return render(request, template_name, {'form':form, 'fair':fair})
 
 #update a company
 def company_update(request, pk, template_name='companies/company_form.html'):
+    fair = current_fair()
     company = get_object_or_404(Company, pk=pk)
     form = CompanyForm(request.POST or None, instance=company)
     if form.is_valid():
         form.save()
         return redirect('companies_list')
-    return render(request, template_name, {'form':form})
+    return render(request, template_name, {'form':form, 'fair':fair})
 
 #delete a company
 def company_delete(request, pk, template_name='companies/company_confirm_delete.html'):
+    fair = current_fair()
     company = get_object_or_404(Company, pk=pk)
     if request.method=='POST':
         company.delete()
         return redirect('companies_list') #redirect back to company list
-    return render(request, template_name, {'object':company})
+    return render(request, template_name, {'object':company, 'fair':fair})
 
 ###COMPANY CONTACT###
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+        exclude = ('user',)
 
 #crate a company contact
 def contact_create(request, pk, template_name='companies/contact_form.html'):
+    redirect_to = request.GET.get('next','')
+    fair = current_fair()
     form = ContactForm(request.POST or None)
     company = get_object_or_404(Company, pk=pk)
     if form.is_valid():
         form.save()
-        return render(request, template_name, {'company':company})
-    return render(request, template_name, {'form':form})
+        if redirect_to:
+            return redirect(redirect_to)
+        return render(request, template_name, {'company':company, 'fair':fair})
+    return render(request, template_name, {'form':form, 'fair':fair})
 
-def contact_state_toggle(request, contact_pk):
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['password1', 'password2']
+
+def contact_state_toggle(request, contact_pk, template_name='companies/user_form.html'):
+    redirect_to = request.GET.get('next','')
+    fair = current_fair()
     contact = get_object_or_404(Contact, pk=contact_pk)
     if contact.active:
         contact.active = False
+        
     else:
+        if not contact.user:
+            form = UserForm(request.POST or None)   
+            print(form)
+            if not form.is_valid():
+                return render(request, template_name, {'form':form, 'fair':fair})
+            user = form.save(commit=False)
+            user.username = contact.email
+            user.email = contact.email
+            user.save()
+            contact.user = user
         contact.active = True
     contact.save()
-    # This should redirect to "EDIT CONTACT" with contact_pkj
-    return redirect('sales')
+    return redirect(redirect_to)
 
 
