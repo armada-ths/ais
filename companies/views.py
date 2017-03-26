a2e958ba054e341a08bbeb1dfe957762b99f6d8a
milvan <marcus.larson@live.com>
2017-03-26 23:49:35 +0200

    Sales can add companies

--- companies/views.py
+++ companies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.forms import UserCreationForm
 
 from companies.models import Company, Contact
@@ -14,7 +15,7 @@ class CompanyForm(ModelForm):
         fields = '__all__'
 
 def current_fair():
-    return get_object_or_404(Fair, year=2017)
+    return get_object_or_404(Fair, current=True)
 
 #list all companies
 def companies_list(request, template_name='companies/companies_list.html'):
@@ -29,12 +30,20 @@ def list_company(request, pk, template_name='companies/list_company.html'):
     return render(request, template_name, {'company':company, 'fair':fair})
 
 #crate a company
+# By keeping raise_exception=False, any user who tries to access this 
+# page gets logged out. If set to True, it crashes as fair.year does not 
+# necessarily exists and then base.html cannot load.
+# TODO: This is bad design which might need fixing
+@permission_required('companies.company.can_add_company', raise_exception=False)
 def company_create(request, template_name='companies/company_form.html'):
     fair = current_fair()
     form = CompanyForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('companies_list')
+        next = request.GET.get('next')
+        if next:
+            return redirect(next)
+        #return redirect('companies:companies_list') #Do not support accessing this separate
     return render(request, template_name, {'form':form, 'fair':fair})
 
 #update a company
