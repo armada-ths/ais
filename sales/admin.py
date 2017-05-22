bf5cc36d494b2f3a9f4ea97187dc79234b827a93
Griffone <thegriffones@gmail.com>
2017-05-22 17:26:17 +0200

    Added BuisnessArea
    Added BuisnessArea field and implemented it in Sales

--- sales/admin.py
+++ sales/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Sale, SaleComment, FollowUp 
+from .models import Sale, SaleComment, FollowUp, BuisnessArea
 
 # Register your models here.
 admin.site.register(Sale)
 admin.site.register(SaleComment)
 admin.site.register(FollowUp)
+admin.site.register(BuisnessArea)
