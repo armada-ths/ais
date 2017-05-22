87112ba07b0ca2dc5a6b496318127f81b93d8bfd
Griffone <thegriffones@gmail.com>
2017-05-22 21:16:10 +0200

    Added BusinessArea
    Added BusinessArea model, integrated it in Sale model and modified Sale view to show BusinessArea

--- sales/admin.py
+++ sales/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Sale, SaleComment, FollowUp 
+from .models import Sale, SaleComment, FollowUp, BusinessArea
 
 # Register your models here.
 admin.site.register(Sale)
 admin.site.register(SaleComment)
 admin.site.register(FollowUp)
+admin.site.register(BusinessArea)
