50b2d6370d5a4bf7767e3429827235a0390d6bda
Griffone <thegriffones@gmail.com>
2017-06-21 12:27:50 +0200

    Implemented initial image upload functionality
    UNTESTED! Working on testing now

--- local_settings.py
+++ local_settings.py
@@ -1,5 +1,8 @@
 from ais.local.settings import *
 
+#MEDIA_ROOT = '/vagrant/media'
+#MEDIA_URL = '/media/'
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
