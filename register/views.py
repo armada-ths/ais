4a9a7e6505a0541981e7ab87ea4975570aac24e8
Emma Bäckström <backstromemma@gmail.com>
2017-07-01 21:52:24 +0200

    Exhibitor form also creates SignupLog object when company accept terms and conditions

--- register/views.py
+++ register/views.py
@@ -141,6 +141,7 @@ def company_update(request, pk, template_name='register/company_form.html'):
 # become an exhibitor via the ExhibitorForm
 def create_exhibitor(request, template_name='register/exhibitor_form.html'):
     currentFair = Fair.objects.get(current = True)
+    contract = SignupContract.objects.get(fair=currentFair, current=True)
     if request.user.is_authenticated():
         contact = Contact.objects.get(user=request.user)
         # make sure user is connected to a 'Contact'
@@ -361,6 +362,17 @@ def create_exhibitor(request, template_name='register/exhibitor_form.html'):
                         create_or_update_order(product, amount)
 
 
+                # Contract agreement
+                def create_signup():
+                    signup = None
+                    try:
+                        signup = SignupLog.objects.get(contact=contact, contract=contract, company = contact.belongs_to)
+                    except SignupLog.DoesNotExist:
+                        signup = SignupLog.objects.create(contact=contact, contract=contract, company = contact.belongs_to, type = 'complete')
+
+                if form.accepting_terms():
+                    create_signup()
+
                 # Everything is done!
                 # Do nothing if form is saved, otherwise redirect and send email
                 save_or_submit = form.save_or_submit()
@@ -384,7 +396,7 @@ def create_exhibitor(request, template_name='register/exhibitor_form.html'):
                         fail_silently=False)    
                     return redirect('anmalan:cr_done')
 
-    return render(request, template_name, {'form': form})
+    return render(request, template_name, {'form': form, 'contract_url': contract.contract.url})
 
 # thank you screen after submission of complete registration
 def cr_done(request, template_name='register/finished_registration.html'):
