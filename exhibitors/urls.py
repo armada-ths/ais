6a83ac22d7d9c7f4fcaaa06be948d3ce311ee3b4
Ellinor Westerberg <ell.westerberg@gmail.com>
2017-09-03 22:10:42 +0200

    Added view for sending the emails

--- exhibitors/urls.py
+++ exhibitors/urls.py
@@ -7,9 +7,13 @@ from companies.models import Contact
 urlpatterns = [
     url(r'^$', views.exhibitors, name='exhibitors'),
 	url(r'^(?P<pk>\d+)/$', views.exhibitor, name='exhibitor'),
-	url(r'^send_receipt_email$', views.send_receipt_email, name='send_receipt_email'),
+
+	url(r'^emails_confirmation/$', views.emails_confirmation, name='emails_confirmation'),
+	url(r'^send_cr_receipts$', views.send_cr_receipts, name='send_cr_receipts'),
+	url(r'^send_emails/$', views.send_emails, name='send_emails'),
+
 	url(r'^(?P<exhibitor_pk>\d+)/order/new$', views.related_object_form(Order,'Order', 'order_delete'), name='order_new'),
-	url(r'^(?P<exhibitor_pk>\d+)/order/(?P<instance_pk>\d+)$', views.related_object_form(Order, 'Order', 'order_delete'), name='order'),
+	url(r'^(?P<exhibitor_pk>\d+)/order/(?P<instane_pk>\d+)$', views.related_object_form(Order, 'Order', 'order_delete'), name='order'),
 	url(r'^(?P<exhibitor_pk>\d+)/order/(?P<instance_pk>\d+)/delete$', views.related_object_delete(Order), name='order_delete'),
 
 	url(r'^(?P<exhibitor_pk>\d+)/banquette_attendant/new$', views.related_object_form(BanquetteAttendant, 'Banquet attendant', 'banquette_attendant_delete'), name='banquette_attendant_new'),
--- exhibitors/views.py
+++ exhibitors/views.py
@@ -19,48 +19,6 @@ def user_can_modify_exhibitor(user, exhibitor):
     return user.has_perm(
         'exhibitors.change_exhibitor') or user in exhibitor.hosts.all() or user in exhibitor.superiors()
 
-def send_receipt_email(request, year):
-    fair = Fair.objects.filter(current=True)            
-    exhibitors = Exhibitor.objects.filter(fair=fair)
-    products = Product.objects.filter(fair=fair)
-
-    for exhibitor in exhibitors:
-        try:
-            contact_email = exhibitor.contact.email
-        except:
-            logging.error("No contact for this exhibitor")
-            continue
-
-        orders =  Order.objects.filter(exhibitor = exhibitor)
-        total_price = 0
-
-
-        orders_info = []
-        #Go thrue orders and get the total price for everything and place the important info as a dictionary in the list orders_info
-        for o in orders:
-            product = o.product
-            price = product.price
-            amount = o.amount
-            total_price += price*amount
-
-            order = {'product' : product.name, 'price' : product.price*amount, 'amount' : o.amount} 
-            orders_info.append(order)
-
-
-        send_mail(
-            'Your orders for Armada',
-            get_template('exhibitors/send_cr_receipt.html').render(({
-                'orders_info' : orders_info,
-                'total_price' : total_price,
-                })
-            ),
-
-            settings.DEFAULT_FROM_EMAIL,
-            [contact_email],
-            fail_silently=False)
-
-    return redirect('exhibitors', year)
-
 
 def exhibitors(request, year, template_name='exhibitors/exhibitors.html'):
     if not request.user.has_perm('exhibitors.view_exhibitors'):
@@ -72,8 +30,6 @@ def exhibitors(request, year, template_name='exhibitors/exhibitors.html'):
         'exhibitors': Exhibitor.objects.prefetch_related('hosts').filter(fair=fair).order_by('company__name'),
         'fair': fair
     })
-
-
 
 
 def exhibitor(request, year, pk, template_name='exhibitors/exhibitor.html'):
@@ -181,6 +137,69 @@ def exhibitor(request, year, pk, template_name='exhibitors/exhibitor.html'):
         'fair': fair
     })
 
+
+#Where the user can chose to send email to all exhibiors with their orders
+def send_emails(request, year, template_name='exhibitors/send_emails.html'):
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+    
+    fair = get_object_or_404(Fair, year=year)
+    return render(request, template_name, {'fair': fair})
+
+#Comfirmation that email has been sent to all exhibitors
+def emails_confirmation(request, year, template_name='exhibitors/emails_confirmation.html'):
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+
+    fair = get_object_or_404(Fair, year=year)   
+    return render(request, template_name, {'fair': fair})
+
+#Sends email to all exhibitors with their orders
+def send_cr_receipts(request, year):
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+
+    fair = get_object_or_404(Fair, year=year)           
+    exhibitors = Exhibitor.objects.filter(fair=fair)
+    products = Product.objects.filter(fair=fair)
+
+
+    for exhibitor in exhibitors:
+        try:
+            contact_email = exhibitor.contact.email
+        except:
+            logging.error("No contact for this exhibitor")
+            continue
+
+        orders =  Order.objects.filter(exhibitor = exhibitor)
+        total_price = 0
+
+
+        orders_info = []
+        #Go thrue orders and get the total price for everything and place the important info as a dictionary in the list orders_info
+        for o in orders:
+            product = o.product
+            price = product.price
+            amount = o.amount
+            total_price += price*amount
+
+            order = {'product' : product.name, 'price' : product.price*amount, 'amount' : o.amount} 
+            orders_info.append(order)
+
+
+        send_mail(
+            'Your orders for Armada',
+            get_template('exhibitors/cr_receipt.html').render(({
+                'orders_info' : orders_info,
+                'total_price' : total_price,
+                })
+            ),
+
+            settings.DEFAULT_FROM_EMAIL,
+            [contact_email],
+            fail_silently=False)
+
+    return render(request, 'exhibitors/emails_confirmation.html', {'fair': fair})
 
 def related_object_form(model, model_name, delete_view_name):
     def view(request, year, exhibitor_pk, instance_pk=None, template_name='exhibitors/related_object_form.html'):
