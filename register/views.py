e7882bc9af44a317d25e3220ba746d359bf2e986
atom <viktorgj@kth.se>
2017-09-04 07:58:45 +0200

    changed to try/except in getTimeFlag

--- register/views.py
+++ register/views.py
@@ -29,21 +29,25 @@ PRODUCT_LOG = ":"
 
 def getTimeFlag(close_offset = 7, warning_offset = 7):
     # used to close cr, a warning text after deadline will pop up, however exhibitors will not be permitted to do any changes after the offset in days has passed
-    currentFair = Fair.objects.get(current=True)
-    if currentFair.complete_registration_close_date:
-        end_time = currentFair.complete_registration_close_date.replace(tzinfo=None)
-        end_time_close = end_time + datetime.timedelta(days=close_offset)
-        time = datetime.datetime.now().replace(microsecond=0)
-        warning_time = end_time - datetime.timedelta(days=warning_offset)
-        if time < end_time and time > warning_time:
-            return('warning', [end_time, end_time - time])
-        elif time > end_time and time < end_time_close:
-            return('overdue', [end_time, time - end_time])
-        elif time > end_time_close:
-            return('closed', [end_time, time - end_time])
+    currentFair = None
+    try:
+        currentFair = Fair.objects.get(current=True)
+        if currentFair.complete_registration_close_date:
+            end_time = currentFair.complete_registration_close_date.replace(tzinfo=None)
+            end_time_close = end_time + datetime.timedelta(days=close_offset)
+            time = datetime.datetime.now().replace(microsecond=0)
+            warning_time = end_time - datetime.timedelta(days=warning_offset)
+            if time < end_time and time > warning_time:
+                return('warning', [end_time, end_time - time])
+            elif time > end_time and time < end_time_close:
+                return('overdue', [end_time, time - end_time])
+            elif time > end_time_close:
+                return('closed', [end_time, time - end_time])
+            else:
+                return(None, [None, None])
         else:
             return(None, [None, None])
-    else:
+    except Fair.DoesNotExist:
         return(None, [None, None])
 
 def index(request, template_name='register/index.html'):
@@ -53,7 +57,6 @@ def index(request, template_name='register/index.html'):
         else:
             return redirect('anmalan:logout')
     timeFlag, [time_end, time_diff] = getTimeFlag()
-    print(time_end)
     return render(request, template_name, {'timeFlag': timeFlag, 'time_end': time_end, 'time_diff': time_diff})
 
 def home(request, template_name='register/home.html'):
