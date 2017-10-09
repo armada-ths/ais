4bdc61b16c02dadeb1866bea65ba2be616c01b54
Griffone <thegriffones@gmail.com>
2017-10-09 19:36:37 +0200

    Switched student_profiles/models.py to spaces for tabs

--- student_profiles/models.py
+++ student_profiles/models.py
@@ -5,7 +5,7 @@ class StudentProfile(models.Model):
     A striped down version of a user profile
     Is used by api and banquet apps
     '''
-	nickname = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
 
-	def __str__(self):
-		return self.nickname
+    def __str__(self):
+        return self.nickname
