3815631ff845e74d74962f77ee183934beb4f2fb
Emma Bäckström <backstromemma@gmail.com>
2017-10-13 00:28:37 +0200

    Changed migrations manually so that ais will never apply ticket_type field

--- banquet/migrations/0010_banquetticket.py
+++ banquet/migrations/0010_banquetticket.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('banquet', '0009_auto_20171010_2105'),
+        ('banquet', '0008_auto_20171007_1355'),
     ]
 
     operations = [
