6f2d36d208830732dc96cf25ef186ece2166719f
Griffone <thegriffones@gmail.com>
2017-10-16 12:06:05 +0200

    Removed CSRF verification from PUT request

--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ import platform, subprocess, json
 from django.contrib.auth.models import Group
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
@@ -136,6 +137,7 @@ def banquet_placement(request):
     return JsonResponse(data, safe=False)
 
 
+@csrf_exempt
 def student_profile(request):
     '''
     GET student profiles nickname by their id.
