177f8b4b6511a2b3ade3cd411fba49f1cf4d94de
atom <viktorgj@kth.se>
2017-10-31 09:21:19 +0100

    commit before merging update on matching model from ellinors branch

--- matching/matching-algorithm/processExhibitorAnswers.py
+++ matching/matching-algorithm/processExhibitorAnswers.py
@@ -1,4 +1,9 @@
 '''
+Preprocessing requires:
+    nltk
+    enchant
+    gensim
+
 Dependencies for gensim:
     pip install numpy
     pip install scipy
@@ -8,4 +13,27 @@ Then do:
 '''
 from django.contrib.auth.models import User
 from fair.models import Fair
-from matching.models import *
+from exhibitors.models import Exhibitor
+from matching.models import Survey, Response, TextAns
+
+import nltk
+import enchant
+import gensim
+
+class SpellChecker():
+
+    def __init__(self, dict_name='en_US', max_dist=3):
+        self.spell_dict = enchant.Dict(dict_name)
+        self.max_dist = max_dist
+
+    def replace(self, word):
+        suggestion = self.spell_dict.suggest(word)
+
+def genWorldRegions(responses, survey_raw, survey_processed):
+    pass
+
+def genWorkFields(responses, survey_raw, survey_processed):
+    '''
+    Objects must be a list of matching.Response model objects
+    '''
+    pass
