3dcfbd4e52fd1d1e546d59ebeda00ec546761e05
Griffone <thegriffones@gmail.com>
2017-11-04 17:55:22 +0100

    Tweaked the migration

--- student_profiles/migrations/0008_auto_20171104_1526.py
+++ student_profiles/migrations/0008_auto_20171104_1526.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='studentprofile',
             name='id',
-            field=models.AutoField(auto_created=True, default=0, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
             preserve_default=False,
         ),
         migrations.AlterField(
