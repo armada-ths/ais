6c685e8986b70075d33dccbb6dc7c61fb5e82179
Adam J <worldyn@users.noreply.github.com>
2017-11-05 18:14:27 +0100

    removed merge errors

--- ais/celery.py
+++ ais/celery.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
@@ -20,13 +19,4 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
   print('Request: {0!r}'.format(self.request))
-=======
-import os
-from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ais.local.settings')
-
-app = Celery('ais')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
->>>>>>> 188af7c6c6ec9f6e6f0dec7b2eb04e0d5ac411dc
