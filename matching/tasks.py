2579877d5883e6b01359efd6baac95648d0188e1
atom <viktorgj@kth.se>
2017-11-06 13:58:53 +0100

    updated classify task

--- matching/tasks.py
+++ matching/tasks.py
@@ -11,13 +11,12 @@ from django.utils.crypto import get_random_string
 #from celery import shared_task
 from celery.decorators import task
 
+from .algorithms import main_classify as classify
+
 @task(name="classify_student")
 def classify_student(student_pk, survey_pk, num_of_results):
-    first_name = 'testbanquet_{}'.format(get_random_string(10, string.ascii_letters))
-    email = '{}@hotmail.com'.format(first_name)
-    last_name = get_random_string(50)
-    BanquetteAttendant.objects.create(first_name=first_name, last_name=last_name, email=email, fair=Fair.objects.get(current=True), gender="male", phone_number="00000")
-    return 'CLASSIFY STUDENT CREATED A BANQUET ATTENDANT WHOHOO!'
+    classify.classify(student_pk, survey_pk, num_of_results)
+    return 'Student got classified!'
 
 @task(name="random_user_creation")
 def create_random_user():
