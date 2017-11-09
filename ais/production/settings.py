390723c085a1965ce2a2e2729a7ee14a48c283d2
atom <viktorgj@kth.se>
2017-11-09 13:54:34 +0100

    added debug true temporarily

--- ais/production/settings.py
+++ ais/production/settings.py
@@ -9,6 +9,8 @@ from os import environ as env
 from ais.common.settings import *
 from ais.secrets import *
 
+DEBUG = True
+
 # This is important so other people can't set their own domains
 # to point to AIS (which would be a security concern).
 ALLOWED_HOSTS = ['.armada.nu', 'localhost']
@@ -95,4 +97,3 @@ LOGGING = {
 ADMINS = MANAGERS = (
    ('System', 'system@armada.nu'),
 )
-
