0abb28737e94a39fd51eeab9d9f54e934e539097
axelinternet <axel@hultman.info>
2017-12-04 18:08:13 +0100

    Changed output to xlsx format

--- scripts/invoice/readable_invoice_exhibitors.py
+++ scripts/invoice/readable_invoice_exhibitors.py
@@ -3,44 +3,46 @@ from exhibitors.models import Exhibitor
 from orders.models import Product, ProductType, Order
 from companies.models import Company, Contact
 from fair.models import Fair
+from openpyxl import load_workbook
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment, colors, Font, Color
 
-# This script generate a readable invoice text file for each exhibitor
+"""
+    This script generate a readable invoice xlsx file for each exhibitor.
+    It includes general exhibitor information and summary of purchased products.
+"""
+
 for exhibitor in Exhibitor.objects.filter(fair=Fair.objects.get(current=True)):
-    with open('invoice_readable/' + exhibitor.company.name + '.txt','w') as f:
-        # File information
-        f.write('FAKTURA FÖR: ' + exhibitor.company.name + '\n')
-        f.write('\n')
-        f.write('------------------------------------------------------------------ \n')
-        f.write('Detta dokument innehåller information kring fakturan samt en lista \n')
-        f.write('av köp med respektive pris, följt av den totala summan av alla köp \n')
-        f.write('OBS: om ett fält i fakturainfot är tomt betyder det att det ej är ifyllt. \n')
-        f.write('------------------------------------------------------------------ \n')
+    wb = load_workbook('scripts/invoice/invoice_template.xlsx')
+    ws = wb.active # Select fakturaunderlag-mall sheet
+    
+    # Assign company info to template
+    ws['E5'] = exhibitor.company.name
+    ws['C8'] = exhibitor.invoice_reference
+    ws['C9'] = 'Project Manager THS Armada'
+    ws['E8'] = exhibitor.invoice_address
+    if exhibitor.invoice_address_po_box:
+        ws['E9'] = exhibitor.invoice_address_po_box
+        ws['E10'] = exhibitor.invoice_address_zip_code        
+    else:
+        ws['E9'] = exhibitor.invoice_address_zip_code
+    ws['A16'] = exhibitor.invoice_additional_information
 
-        # Invoice information
-        f.write('\n')
-        f.write('INFORMATION: \n\n')
-        f.write('Fakturareferens: ' + exhibitor.invoice_reference + '\n')
-        f.write('Köpordernummer: ' + exhibitor.invoice_purchase_order_number + '\n')
-        f.write('Referens telefon: ' + exhibitor.invoice_reference_phone_number + '\n')
-        f.write('Organisationsnamn: ' + exhibitor.invoice_organisation_name + '\n')
-        f.write('Adress: ' + exhibitor.invoice_address + '\n')
-        f.write('Postbox: ' + exhibitor.invoice_address_po_box + '\n')
-        f.write('Postnummer: ' + exhibitor.invoice_address_zip_code + '\n')
-        f.write('Fakturaidentifikation: ' + exhibitor.invoice_identification + '\n')
-        f.write('Övrig info: ' + exhibitor.invoice_additional_information + '\n')
-        f.write('\n')
-        f.write('------------------------------------------------------------------ \n')
-        f.write('\n')
+    current_row = 23
+    for order in Order.objects.filter(exhibitor=exhibitor):
+        ws['A' + str(current_row)] = order.amount
+        ws['B' + str(current_row)] = order.product.name
+        ws['E' + str(current_row)] = order.product.price
+        ws['G' + str(current_row)] = order.product.coa_number
+        ws['H' + str(current_row)] = 11 # Resultatställe armada
+        ws['J' + str(current_row)] = 0 # Resultatställe armada
+        current_row += 1
 
-        # Order list
-        f.write('KÖPLISTA: \n\n')
-        for order in Order.objects.filter(exhibitor=exhibitor):
-            f.write(order.product.name + ': ' + str(order.amount) + ' x ' + str(order.product.price) + ' = ' + str(order.price()) + '\n')
-        f.write('\n')
-        f.write('------------------------------------------------------------------ \n')
-        f.write('\n')
 
-        # Total sum
-        f.write('TOTALA SUMMA: \n')
-        f.write(str(exhibitor.total_cost()) + ' SEK \n\n')
-        f.write('------------------------------------------------------------------ \n')
+    # Ugly styling hack to reset stylings
+    ft = Font(name='Garamond',
+                size=19,
+                bold=True,
+                color='000066')
+
+    wb.save('scripts/invoice/{0}.xlsx'.format(exhibitor.company.name))
+    print('Successfully wrote invoices to file')
