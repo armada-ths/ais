4fb41411fa730d5822446a8f5a14e8bc89fc76c2
milvan <marcus.larson@live.com>
2018-01-10 19:51:40 +0100

    missing on_delete rules

--- orders/models.py
+++ orders/models.py
@@ -16,13 +16,13 @@ class ProductType(models.Model):
 
 # A 'Product' is a purchasable item that belongs to a 'Fair' and has a 'ProductType'
 class Product(models.Model):
-    fair = models.ForeignKey('fair.Fair')
+    fair = models.ForeignKey('fair.Fair', on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
     # Chart of accounts number (sv. kontonummer), used for accounting
     coa_number = models.PositiveSmallIntegerField()
     price = models.IntegerField()
-    product_type = models.ForeignKey(ProductType, null=True, blank=True)
+    product_type = models.ForeignKey(ProductType, null=True, blank=True, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['name']
@@ -42,7 +42,7 @@ class Product(models.Model):
 class Order(models.Model):
     exhibitor = models.ForeignKey(
         'exhibitors.Exhibitor', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.PositiveSmallIntegerField()
 
     class Meta:
