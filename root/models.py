4b117519ea39b20457495f3ff11e246194e8c57a
Martin Pola <martin.pola@armada.nu>
2018-04-07 01:39:32 +0200

    fix: display users' real names by default

--- root/models.py
+++ root/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import Group
-from fair.models import Fair
+from django.contrib.auth.models import User
 
-# Create your models here.
+def get_common_name(self):
+	return "%s %s" % (self.first_name, self.last_name) if self.first_name and self.last_name else self.username
 
+User.add_to_class("__str__", get_common_name)
