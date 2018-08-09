2f85a7e96402e0454dca7ab370a485ed06a8e755
Martin Pola <martin.pola@armada.nu>
2018-08-09 18:48:10 +0300

    Armada project members can now also see the registration
    This adds a button to the CRM from which Armada project members can see what the registration page looks like for the companies.

--- register/urls.py
+++ register/urls.py
@@ -6,7 +6,7 @@ from .forms import LoginForm, ResetPasswordForm, SetNewPasswordForm
 app_name = 'anmalan'
 urlpatterns = [
     url(r'^$', views.choose_company, name = 'choose_company'),
-    url(r'^(?P<company_contact_pk>[0-9]+)$', views.form, name = 'form'),
+    url(r'^(?P<company_pk>[0-9]+)$', views.form, name = 'form'),
     url(r'^submitted/$', views.submission_view, name='submitted'),
     url(r'^signup', views.signup, name='create_company_user'),
     url(r'^new_company', views.create_company, name='create_company'),
