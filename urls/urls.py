a2a956f7b9eb0546557fbd833364fecd0fa8d8b3
juzov <ejuzov@gmail.com>
2018-08-29 12:38:01 +0000

    Removed people list and product

--- urls/urls.py
+++ urls/urls.py
@@ -4,12 +4,11 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('root.urls')),
     url(r'^events/', include('events.urls')),
+    url(r'^people/', include('people.urls')),
     url(r'^locations/', include('locations.urls')),
     url(r'^news/', include('news.urls')),
-    url(r'^people/', include('people.urls')),
     url(r'^recruitment/', include('recruitment.urls')),
     url(r'^exhibitors/', include('exhibitors.urls')),
-    url(r'^products/', include('orders.urls')),
     url(r'^companies/', include('companies.urls')),
     url(r'^accounting/', include('accounting.urls')),
     url(r'^banquet/', include('banquet.urls')),
