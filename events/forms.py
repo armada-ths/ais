7fe63ffbffdbabda046e32b70cf9d5f033608cdf
Henry Bogaeus <henryb@spotify.com>
2018-09-18 16:24:59 +0200

    Even more work on events

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,15 @@
-from django.forms import ModelForm, Textarea
-from events.models import Event
+from django.forms import ModelForm
+
+from events.models import Event, Team
 
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = "__all__"
+
+
+class TeamForm(ModelForm):
+    class Meta:
+        model = Team
+        fields = ['name', 'max_capacity', 'allow_join_cr', 'allow_join_s']
