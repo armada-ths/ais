08a3dc3849350055c5701811fd2e8c6b9c278c20
Martin Pola <martin.pola@armada.nu>
2018-10-05 09:59:35 +0200

    removed unused files

--- restart_uwsgi_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while read line
-do
-    kill -HUP $line
-done </home/deployment/ais-master.pid
--- start_uwsgi_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/bin/bash -c "
-    . /home/deployment/ais_venv/bin/activate;
-    cd ~/ais;
-    uwsgi --ini ais_uwsgi.ini
-"
