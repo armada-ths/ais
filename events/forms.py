35048e02dbe21fa692a1a8ba74aa1fe0d1be4558
Martin Pola <martin.pola@armada.nu>
2018-10-23 17:12:30 +0200

    make it clear on which form we expect dates and times in

--- events/forms.py
+++ events/forms.py
@@ -1,12 +1,16 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 from events.models import Event, Team
 
 
 class EventForm(ModelForm):
-    class Meta:
-        model = Event
-        fields = "__all__"
+	class Meta:
+		model = Event
+		fields = '__all__'
+		widgets = {
+			'date_start': TextInput(attrs = {'placeholder': '1994-07-12 13:37:00'}),
+			'date_end': TextInput(attrs = {'placeholder': '1995-10-10 13:37:00'})
+		}
 
 
 class TeamForm(ModelForm):
