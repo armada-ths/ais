88bef545b087a3b398ac0d245b99b397ef7e2675
Martin Pola <martin.pola@armada.nu>
2018-11-11 13:17:19 +0100

    improved fair admin

--- fair/admin.py
+++ fair/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Fair, FairDay, Partner, Tag, OrganizationGroup, LunchTicketTime, LunchTicket
 
-admin.site.register(Fair)
+class FairDayAdmin(admin.TabularInline):
+	model = FairDay
+
+class FairAdmin(admin.ModelAdmin):
+	inlines = [FairDayAdmin]
+
+admin.site.register(Fair,FairAdmin)
+
 admin.site.register(Partner)
 admin.site.register(Tag)
 admin.site.register(OrganizationGroup)
