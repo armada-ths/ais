384341416ffa91243621884f814b079bb79c22db
Sara Gustafsson <sara.gustafsson@armada.nu>
2019-07-21 13:55:51 +0000

    show include_in_form in admin list

--- exhibitors/admin.py
+++ exhibitors/admin.py
@@ -11,12 +11,36 @@ class ExhibitorAdmin(admin.ModelAdmin):
 	ordering = ['-fair__year', 'company']
 	list_filter = ['fair']
 
+@admin.register(CatalogueIndustry)
+class CatalogueIndustryAdmin(admin.ModelAdmin):
+	list_display = ('industry', 'include_in_form')
+	list_filter = ['include_in_form']
+	ordering = ['industry']
 
-admin.site.register(CatalogueIndustry)
-admin.site.register(CatalogueValue)
-admin.site.register(CatalogueEmployment)
-admin.site.register(CatalogueLocation)
-admin.site.register(CatalogueBenefit)
+@admin.register(CatalogueValue)
+class CatalogueValueAdmin(admin.ModelAdmin):
+	list_display = ('value', 'include_in_form')
+	list_filter = ['include_in_form']
+	ordering = ['value']
+
+@admin.register(CatalogueEmployment)
+class CatalogueEmploymentAdmin(admin.ModelAdmin):
+	list_display = ('employment', 'include_in_form')
+	list_filter = ['include_in_form']
+	ordering = ['employment']
+
+@admin.register(CatalogueLocation)
+class CatalogueLocationAdmin(admin.ModelAdmin):
+	list_display = ('location', 'include_in_form')
+	list_filter = ['include_in_form']
+	ordering = ['location']
+
+@admin.register(CatalogueBenefit)
+class CatalogueBenefitAdmin(admin.ModelAdmin):
+	list_display = ('benefit', 'include_in_form')
+	list_filter = ['include_in_form']
+	ordering = ['benefit']
+
 admin.site.register(Location)
 admin.site.register(Booth)
 admin.site.register(ExhibitorInBooth)
