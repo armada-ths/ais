780ef9cde41280a6bdcd0ce4e11dccb34d5a6d3f
Sara Gustafsson <sara.gustafsson@armada.nu>
2019-07-21 17:47:26 +0000

    attach file test

--- testpage/views.py
+++ testpage/views.py
@@ -168,6 +168,6 @@ Product --- Quantity --- Unit price (SEK) --- Product total (SEK)
     #file_path = os.path.join(settings.MEDIA_ROOT, signature.contract.contract.url)
     file_path = settings.MEDIA_ROOT + signature.contract.contract.url
     print("Contract path: ", file_path)
-    #email.attach_file(file_path)
+    email.attach_file(file_path)
 
     email.send()
