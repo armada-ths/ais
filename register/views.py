a9fc40b012d264785e53b335ac0e83562254eb5c
Sara Gustafsson <sara.gustafsson@armada.nu>
2019-07-21 16:30:33 +0000

    send multialternative email

--- register/views.py
+++ register/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from django.contrib.auth import authenticate, login, update_session_auth_hash
 from django.conf import settings
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import get_template
 from django.forms.models import inlineformset_factory, HiddenInput
@@ -50,86 +50,108 @@ def get_contract(company, fair, registration_type):
 
 
 def send_CR_confirmation_email(signature, orders, orders_total):
-	order_table_rows = []
-	for order in orders:
-		if order['category']:
-			product = order['category'] + ' - ' + order['name']
-		else:
-			product = order['name']
 
-		order_row = '''
-						<tr>
+    order_html_rows = []
+    order_plain_rows = []
+
+    for order in orders:
+        if order['category']:
+            product = order['category'] + ' - ' + order['name']
+        else:
+            product = order['name']
+
+        html_row = '''  <tr>
 						<td>%s</td>
 						<td>%s</td>
 						<td>%s</td>
 						<td>%s</td>
 						</tr>
-					''' % (product, order['quantity'], order['unit_price'], str(int(order['quantity'])*int(order['unit_price'])))
-		order_table_rows.append(order_row)
+                    ''' % (product, str(order['quantity']), str(order['unit_price']), str(int(order['quantity'])*int(order['unit_price'])))
 
-	html_message = '''
+        plain_row = '%s --- %s --- %s --- %s' % (product, order['quantity'], order['unit_price'], str(int(order['quantity'])*int(order['unit_price'])))
+
+        order_html_rows.append(html_row)
+        order_plain_rows.append(plain_row)
+
+    html_message = '''
 		<html>
-			<head>
-				<title></title>
-			</head>
-			<body>
-				<style>
-					* {
-					  font-family: sans-serif;
-					  font-size: 12px;
-					}
-				</style>
-				<div>
-					Thank you for submitting the complete registration for THS Armada 2019. The complete registration contract was signed by %s on the %s for %s.
-					<br/><br/>
-					Please note that this is an automatically generated email. If you have any questions please contact your sales contact person. For contact information, please visit our <a href="https://armada.nu/contact/">website</a>.
-				</div>
-				<div>
-					<br/>
-					To view or update your choices visit your <a href="http://ais.armada.nu/register/%s/registration">registration page</a>.
-					<br/>
-					Your current order contains the products listed below.
-					<br/>
-					Total price: SEK %s
-					<br/><br/>
-					<table align="left" border="1" cellpadding="1" cellspacing="0" style="width:600px">
-						<thead>
-							<tr>
-								<th scope="col">Product</th>
-								<th scope="col">Quantity</th>
-								<th scope="col">Unit price (SEK)</th>
-								<th scope="col">Total price (SEK)</th>
-							</tr>
-						</thead>
-						<tbody>
-							%s
-						</tbody>
-					</table>
-				</div>
-			</body>
-		</html>
+        	<body>
+        		<style>
+        			* {
+        			  font-family: sans-serif;
+        			  font-size: 12px;
+        			}
+        		</style>
+        		<div>
+        			Thank you for submitting the complete registration for THS Armada 2019. The complete registration contract was signed by %s on the %s for %s.
+        			<br/><br/>
+        			Please note that this is an automatically generated email. If you have any questions please contact your sales contact person. For contact information, please visit <a href="https://armada.nu/contact/">armada.nu/contact</a>.
+        		</div>
+        		<div>
+        			<br/>
+        			Your current order contains the products listed below.
+        			<br/>
+        			Total amount: SEK %s
+        			<br/>
+        			To view or update your choices go to <a href="https://ais.armada.nu/register/">register.armada.nu</a>.
+        			<br/><br/>
+        			<table align="left" border="1" cellpadding="1" cellspacing="0" style="width:600px">
+        				<thead>
+        					<tr>
+        						<th scope="col">Product</th>
+        						<th scope="col">Quantity</th>
+        						<th scope="col">Unit price (SEK)</th>
+        						<th scope="col">Product total (SEK)</th>
+        					</tr>
+        				</thead>
+        				<tbody>
+        					%s
+        				</tbody>
+        			</table>
+        		</div>
+        	</body>
+        </html>
 		''' % 	(
 				str(signature.company_contact),
-				str(signature.timestamp),
+				str(signature.timestamp.strftime('%Y-%m-%d (%H:%M)')),
 				str(signature.company),
-				str(signature.company.pk),
 				str(orders_total),
-				''.join(order_table_rows),
+				''.join(order_html_rows)
 				)
 
-	email = EmailMessage(
-		subject = 'Registration for THS Armada 2019',
-		body = html_message,
-		from_email = 'info@armada.nu',
-		to = [signature.company_contact.email_address],
-		# bcc = [],
-	)
-	email.content_subtype = 'html'
+    plain_text_message = '''Thank you for submitting the complete registration for THS Armada 2019. The complete registration contract was signed by %s on the %s for %s.
 
-	# file_path = 'https://ais.armada.nu' + signature.contract.contract.url
-	# print("Contract path: ", file_path)
-	# email.attach_file(signature.contract.contract.url)
-	email.send()
+Please note that this is an automatically generated email. If you have any questions please contact your sales contact person. For contact information, please visit https://armada.nu/contact/.
+
+Your current order contains the products listed below.
+Total amount: SEK %s
+To view or update your choices go to https://register.armada.nu.
+
+Product --- Quantity --- Unit price (SEK) --- Product total (SEK)
+%s
+''' % 	(
+				str(signature.company_contact),
+				str(signature.timestamp.strftime('%Y-%m-%d (%H:%M)')),
+				str(signature.company),
+				str(orders_total),
+				'\n'.join(order_plain_rows),
+				)
+
+    email = EmailMultiAlternatives(
+        'Registration for THS Armada 2019',
+        plain_text_message,
+        'info@armada.nu',
+        ['noreply@armada.nu'],
+        #bcc = [],
+    )
+
+    email.attach_alternative(html_message, 'text/html')
+
+    file_path = 'https://ais.armada.nu' + signature.contract.contract.url
+    # print("Contract path: ", file_path)
+    # email.attach_file(signature.contract.contract.url)
+
+    email.send()
 
 
 def choose_company(request):
@@ -413,7 +435,7 @@ def form_complete(request, company, company_contact, fair, exhibitor):
 	if exhibitor.electricity_socket_count is None: errors.append('Number of power sockets required')
 	if not exhibitor.catalogue_about: errors.append('Short text about the company')
 	if not exhibitor.catalogue_purpose: errors.append('Text about the purpose of the company')
-	if not exhibitor.catalogue_logo_squared: errors.append('Squared logotype')
+	#if not exhibitor.catalogue_logo_squared: errors.append('Squared logotype')
 
 	if signature:
 		for field in form_company_details.fields: form_company_details.fields[field].disabled = True
