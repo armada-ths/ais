bc654dda8ef851b5a207bf951629c11b0237eec3
Sara Gustafsson <sara.gustafsson@armada.nu>
2019-07-22 17:52:12 +0000

    make invoice details editable after signature

--- register/views.py
+++ register/views.py
@@ -148,7 +148,7 @@ Product --- Quantity --- Unit price (SEK) --- Product total (SEK)
     email.attach_alternative(html_message, 'text/html')
 
     file_path = settings.MEDIA_ROOT + signature.contract.contract.url[6:]
-    email.attach_file(file_path)
+    email.attach_file(file_path) # NOTE: comment out if run locally
 
     email.send()
 
@@ -435,10 +435,7 @@ def form_complete(request, company, company_contact, fair, exhibitor):
 	if exhibitor.electricity_socket_count is None: errors.append('Number of power sockets required')
 	if not exhibitor.catalogue_about: errors.append('Short text about the company')
 	if not exhibitor.catalogue_purpose: errors.append('Text about the purpose of the company')
-	#if not exhibitor.catalogue_logo_squared: errors.append('Squared logotype')
-
-	if signature:
-		for field in form_company_details.fields: form_company_details.fields[field].disabled = True
+	if not exhibitor.catalogue_logo_squared: errors.append('Squared logotype')
 
 	if not is_editable:
 		for field in form_company_details.fields: form_company_details.fields[field].disabled = True
