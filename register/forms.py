cb18c1657552c65ccd7b11cf8c0abda32c3a0f31
Sara Gustafsson <sara.gustafsson@armada.nu>
2019-07-27 09:48:40 +0000

    updated transport info

--- register/forms.py
+++ register/forms.py
@@ -147,7 +147,7 @@ class CompleteLogisticsDetailsFormWithCheckbox(CompleteLogisticsDetailsForm):
 		fields = ['confirmation_box'] + CompleteLogisticsDetailsForm.Meta.fields
 
 	confirmation_box = forms.BooleanField(required = False,
-		label='I have read the relevant information above and I am aware of the services offered.')
+		label = 'I have read the information regarding booth equipment and transportation above and I am aware of the services offered.')
 
 	def is_valid(self):
 		valid = super(CompleteLogisticsDetailsFormWithCheckbox, self).is_valid()
