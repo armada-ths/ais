fafd62e5545041759f0ae3d6eb241ea74c795d65
Johanna Iivanainen <johanna.ii@outlook.com>
2019-09-11 20:15:50 +0000

    Added a text to one of the fields in the export view and added a attribute e-invoive

--- companies/models.py
+++ companies/models.py
@@ -108,6 +108,7 @@ class Company(models.Model):
 	invoice_country = models.CharField(max_length = 200, choices = countries, default = 'SWEDEN', null = True, blank = True)
 	invoice_reference = models.CharField(max_length = 300, null = True, blank = True)
 	invoice_email_address = models.CharField(max_length = 300, null = True, blank = True, verbose_name = 'Invoice e-mail address')
+	e_invoice = models.IntegerField(verbose_name = 'E-invoice', default = 0)
 	modified_by = None
 
 	def has_invoice_address(self):
