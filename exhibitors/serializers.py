0129cf8ba39c4d83d717ad8a9ecf2a190c11b621
Svante Sörberg <svantesorberg@outlook.com>
2019-10-01 17:35:46 +0000

    Updated yet another serialization variant for exhibitors...

--- exhibitors/serializers.py
+++ exhibitors/serializers.py
@@ -22,6 +22,8 @@ def exhibitor(request, exhibitor, company):
         'values': [{'id': value.pk, 'name': value.value} for value in exhibitor.catalogue_values.all()],
         'employments': [{'id': employment.pk, 'name': employment.employment} for employment in exhibitor.catalogue_employments.all()],
         'locations': [{'id': location.pk, 'name': location.location} for location in exhibitor.catalogue_locations.all()],
+        'competences': [{'id': competence.pk, 'name': competence.competence} for competence in exhibitor.catalogue_competences.all()],
+        'cities': exhibitor.catalogue_cities if exhibitor.catalogue_cities is not None else '',
         'benefits': [{'id': benefit.pk, 'name': benefit.benefit} for benefit in exhibitor.catalogue_benefits.all()],
         'average_age': exhibitor.catalogue_average_age,
         'founded': exhibitor.catalogue_founded,
