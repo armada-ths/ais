8c71e20468c30c6fd4fd8ec1b0b4f041fc10d9ab
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2020-11-08 19:14:03 +0000

    Change KTH api endpoint

--- lib/KTH_Catalog.py
+++ lib/KTH_Catalog.py
@@ -1,5 +1,11 @@
 import subprocess, base64
+
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+from people.models import Profile
+
 
 def lookup_user(kth_id):
 	"""
@@ -49,20 +55,63 @@ def lookup_user_with_ldap(kth_id):
 
 	return user_info
 
+
 def lookup_user_with_api(kth_id):
 	# KTH Profiles API - Swagger: https://api.kth.se/api/profile/swagger/?url=/api/profile/swagger.json#/v1.1/getPublicProfile_v11
-	base_uri = "https://api.kth.se/api/profile/1.1/"
-
-	uri = base_uri + kth_id
-	response = requests.get(uri)
+	# At this moment, not all the users are found due to an issue in the Profile API. We use kottnet API in the meantime.
+	# base_uri = "https://api.kth.se/api/profile/1.1/"
+	base_uri = "https://api.kottnet.net/kth/"
 	
-	if response.status_code // 100 != 2 or len(response.content) == 0:
-		print('failed to connect to KTH Profiles API')
-		return None
+	uri = base_uri + kth_id
 
-	try: 
+	retry_strategy = Retry(total=2, status_forcelist=[429, 500, 502, 503, 504], method_whitelist=["GET"])
+	adapter = HTTPAdapter(max_retries=retry_strategy)
+	sess = requests.Session()
+	sess.mount("https://", adapter)
+
+	try:
+		response = sess.get(uri)
 		json = response.json()
+
+		if response.status_code == 404:
+			print('student not found')
+			return None
+
+		if response.status_code // 100 != 2 or len(response.content) == 0:
+			print('failed to connect to KTH Profiles API')
+			return None
 	except:
 		return None
 
-	return dict(email=json.get('email'), first_name=json.get('givenName'), last_name=json.get('familyName'))
+	email = json.get('username')
+	if email is not None:
+		email += "@kth.se"
+		
+	full_name = json.get('name')
+	first_name = None
+	last_name = None
+	if full_name is not None:
+		full_name = full_name.split(' ', 1)
+		first_name = full_name[0]
+		last_name = full_name[1]
+
+	return dict(email=email, first_name=first_name, last_name=last_name)
+
+
+def merge_user_info(user, info):
+	"""
+	Merge user personal details with information retrieved from their KTH account and returns a confirmation of the operation.
+	"""
+	if info is not None and Profile.objects.filter(user = user, kth_synchronize = False).count() == 0:
+		if info['first_name'] is not None:
+			user.first_name = info['first_name']
+		
+		if info['last_name'] is not None:
+			user.last_name = info['last_name']
+
+		if info['email'] is not None and (user.email is None or len(user.email) == 0 or user.email.endswith('@kth.se')):
+			user.email = info['email']
+
+		return True
+
+	return False
