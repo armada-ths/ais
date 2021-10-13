736ab666c8e06c7131127a4c2a8669a0c6c6bce4
mattiasstahre <stahr@kth.se>
2021-10-13 13:19:50 +0200

    update

--- crm/serializers.py
+++ crm/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 
-from companies.models import Company
+from companies.models import Company, CompanyLog
+
+class CompanySerializerLog(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CompanyLog
+        fields = ('data')
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    
+    companyLog = CompanySerializerLog(many=True, read_only=True)
+
     class Meta:
         model = Company
-        fields = ('name', 'website', 'general_email_address', 'invoice_city')
+        fields = ('name', 'website', 'general_email_address', 'invoice_city', 'companyLog')
+
+
+
