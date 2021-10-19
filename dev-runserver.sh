126a03c2e51d44e0557ba79f1127e924a5024e1a
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-10-19 13:19:04 +0200

    Run server as root

--- dev-runserver.sh
+++ dev-runserver.sh
@@ -2,4 +2,4 @@
 cd /vagrant
 npm run build &&
 npm run watch &
-/vagrant/ais_venv/bin/python manage.py runserver 0.0.0.0:8080 --settings local_settings
+sudo /vagrant/ais_venv/bin/python manage.py runserver 0.0.0.0:8080 --settings local_settings
