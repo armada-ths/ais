c1816d039c68cc1446c3835f6764c2859c3481e0
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2021-10-19 14:47:12 +0200

    #711: revert dev-runserver

--- dev-runserver.sh
+++ dev-runserver.sh
@@ -2,4 +2,4 @@
 cd /vagrant
 npm run build &&
 npm run watch &
-sudo /vagrant/ais_venv/bin/python manage.py runserver 0.0.0.0:8080 --settings local_settings
+/vagrant/ais_venv/bin/python manage.py runserver 0.0.0.0:8080 --settings local_settings
