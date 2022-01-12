da5e77ccb280c0fd0792cb583ce1f7d8797e11e7
arifjehoh <arifjeh.oh@gmail.com>
2022-01-12 23:26:00 +0100

    Docker for ais

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py flush --no-input --settings=local_settings
+# python manage.py migrate --settings=local_settings
+
+exec "$@"
+
