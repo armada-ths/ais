2b188565b9a82f1326d8e89106edc8ab664b813a
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2022-01-24 22:57:54 +0100

    #724: Depend more on .env file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $DB_HOST $DB_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
-python manage.py runserver 0.0.0.0:3000 --settings=local_settings
+python manage.py runserver 0.0.0.0:3000 # AWS PORT
 # python manage.py flush --no-input --settings=local_settings
 # python manage.py migrate --settings=local_settings
 
