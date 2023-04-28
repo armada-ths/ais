3aa1e86694d243ab5086bd4bb1b1f53a7c228636
Didrik Munther <dmu0817@gmail.com>
2023-04-28 07:54:37 +0200

    Format migration files

--- banquet/migrations/0046_auto_20230427_1920.py
+++ banquet/migrations/0046_auto_20230427_1920.py
@@ -4,20 +4,23 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('banquet', '0045_auto_20210918_2247'),
+        ("banquet", "0045_auto_20210918_2247"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='banquet',
-            name='afterparty_price',
-            field=models.PositiveIntegerField(default=0, verbose_name='After Party Price (SEK)'),
+            model_name="banquet",
+            name="afterparty_price",
+            field=models.PositiveIntegerField(
+                default=0, verbose_name="After Party Price (SEK)"
+            ),
         ),
         migrations.AlterField(
-            model_name='banquet',
-            name='afterparty_price_discount',
-            field=models.PositiveIntegerField(default=0, verbose_name='After Party Discounted Price (SEK)'),
+            model_name="banquet",
+            name="afterparty_price_discount",
+            field=models.PositiveIntegerField(
+                default=0, verbose_name="After Party Discounted Price (SEK)"
+            ),
         ),
     ]
