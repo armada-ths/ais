e69af3c9bafd1ba6a05c2a00dad8432462f32d3d
Didrik Munther <dmu0817@gmail.com>
2023-05-22 21:34:19 +0200

    Remove allowed hosts

--- ais/production/settings.py
+++ ais/production/settings.py
@@ -8,9 +8,6 @@ import os
 from ais.common.settings import *
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# This is important so other people can't set their own domains
-# to point to AIS (which would be a security concern).
-ALLOWED_HOSTS = [".armada.nu", "localhost", "armada.nu"]
 
 DEBUG = False
 
