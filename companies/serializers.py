22457a7e85652654c429f85cc1780fcb27beaf7a
Didrik Munther <dmu0817@gmail.com>
2023-08-09 19:03:08 +0200

    Change company serializer

--- companies/serializers.py
+++ companies/serializers.py
@@ -21,37 +21,7 @@ COMPANY_FIELDS = (
     "e_invoice",
 )
 
-
-# Todo 2023 (Didrik Munther): find out which fields should be
-# read only in the IR serializer
-class CompanyIRSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
+class CompanySerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = COMPANY_FIELDS
-        read_only_fields = (
-            "id",
-            "name",
-            "identity_number",
-            "website",
-            "general_email_address",
-            "invoice_name",
-            "invoice_address_line_1",
-            "invoice_address_line_2",
-            "invoice_address_line_3",
-            "invoice_city",
-            "invoice_zip_code",
-            "invoice_country",
-            "invoice_reference",
-            "invoice_email_address",
-            "e_invoice",
-        )
-
-
-class CompanyCRSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = COMPANY_FIELDS
-        read_only_fields = (
-            "id",
-            "name",
-        )
