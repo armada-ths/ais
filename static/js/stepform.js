bbf9624dc207c8bd82bdc5908e305d97637af425
Ferran Campos <ferran98campos@gmail.com>
2023-08-09 21:24:25 +0200

    Merging of company registration form into the user registration form
    The form in /register/company has been 'fused' with the one in /register/user
    
    Before, if users could not find their company in /register/user, they had to click on a link which would bring them to /register/company. There, they could fill a form (very similar to the one in /register/user) in order to register their user and their company into our DB.
    
    Since both forms are relatively similar, this fusion has been done so that users find everything more handy, with the goal of making the registration process easier for them.
    
    Now, if the company is not displayed in the browser, users can fill their company information in /register/user as an alternative to selecting a company from the browser.

--- static/js/stepform.js
+++ static/js/stepform.js
@@ -4,6 +4,7 @@ let numberSteps;
 let nextButton;
 let backButton;
 let submitButton;
+let registerCompanyButton;
 let formSteps;
 let browser;
 let companyTitle;
@@ -11,9 +12,14 @@ let list;
 let writingtimer;
 let stepTrackerChildren;
 let requiredElementsForm;
+let requiredElementsRegisterCompanyForm;
 let form;
+let companyRegistrationForm;
+let companyRegistrationInputs;
 let timeout;
 let noResults;
+let collapsibleButtons;
+let companyRegistration;
 
 function init() {
     //Get elements from DOM
@@ -22,19 +28,25 @@ function init() {
     nextButton = document.getElementById('next-step-btn');
     backButton = document.getElementById('back-step-btn');
     submitButton = document.getElementById('submit-form-btn');
+    registerCompanyButton = document.getElementById('register-company-btn');
     browser = document.getElementById('browser');
     companyTitle = document.getElementById('company-confirmation-title');
     list = document.getElementById('company-list');
     stepTrackerChildren = document.getElementById('form-step-tracker').children;
     form = document.getElementById('user-registration-form');
+    companyRegistrationForm = document.getElementById('company-regitration-form');
+    companyRegistrationInputs = document.getElementById('company-registration-inputs');
     noResults = document.getElementById('company-list-no-results');
+    collapsibleButtons = document.getElementsByClassName('collapsible-preview');
 
     //Setting static initial values
     currentStep = 0;
     browser.placeholder = 'Type your company name...';
     writingtimer = 500
     requiredElementsForm = [];
+    requiredElementsRegisterCompanyForm = [];
     timeout = null;
+    companyRegistration = false; //If false (default), the script will send the user registration form with an existing company form. Otherwise, if true, willregister a new company and the user under that company
 
     //Remove class 'no-transition' from all trackers. 'no-transition' is a CSS class that prevents animations from happening. Elements in DOM are initialized with it 
     //to not execute animations when loading, which would effect at the aesthetics of the webpage. Once they are load, animations can now be played.
@@ -43,9 +55,30 @@ function init() {
     }
 
     //Creation of an array o arrays that contains all the 'required' form input tags of each step. Will be later used for validation purposes
+    let slice = [];
     for (let i = 0; i < formSteps.length; i++) {
-        let slice = [...Array.prototype.slice.call(formSteps[i].querySelectorAll("[required]"))];
-        requiredElementsForm = [...requiredElementsForm, slice];
+        //Distribute these elements in:
+        // - requiredElementsForm => list of required elements of the form in case the company is already registered (company found using browser)
+        // - requiredElementsRegisterCompanyForm => list of required elements of the form in case the company is not registered (currently ALL except the input with id = 'browser)
+        slice = [...Array.prototype.slice.call(formSteps[i].querySelectorAll("[required]"))];
+
+        requiredElementsForm = [...requiredElementsForm, slice.filter(element => !companyRegistrationForm.contains(element))];
+        requiredElementsRegisterCompanyForm = [...requiredElementsRegisterCompanyForm, slice.filter(element => element.id != 'browser')];
+    }
+
+    //Add an event listener to all collapsibles. This is so collapse or show the content depending on the content current height
+    for (let i = 0; i < collapsibleButtons.length; i++) {
+        collapsibleButtons[i].addEventListener('click', function (event) {
+            //If the height is greater than 0, we collapse the content. Otherwise we show it
+            let content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                event.target.classList.remove("active");
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                event.target.classList.add("active");
+            }
+        });
     }
 }
 
@@ -94,6 +127,7 @@ function setActiveStep() {
         //Shows the browser and hides the 'company confirmation' page
         document.getElementById('company-confirmation').style.display = 'none';
         document.getElementById('company-search').style.display = 'block';
+        companyRegistrationForm.style.display = 'block';
         setVisibleButtons(false, false, false, false);
 
     } else if (currentStep == numberSteps - 1) {
@@ -102,14 +136,36 @@ function setActiveStep() {
         setVisibleButtons(true, true, false, false);
     }
 
+    //Erase company information depending on case scenario when jumping from step 0 to step 1 in order to avoid the form submitting both
+    //types of information (since one path excludes the other)
+    eraseCompanyInformation();
     //Color the steps to green to indicate the user in which step its currently in
     colorStepNumber();
 }
 
-function validateStep(stepNumber) {
+function eraseCompanyInformation() {
+    //If the user submits a company name, all the inputed value inside the collapsible is erased. Otherwise, if the user clicks on the 'register company' button, 
+    //the company name provided is erased. This is done in order to minimize user errors
+    if (companyRegistration) {
+        browser.value = "";
+    } else {
+        [...companyRegistrationInputs.querySelectorAll("input")].forEach(element => {
+            element.value = "";
+        });
+    }
+}
+
+function validateStep(stepNumber, requiredElementsForm, jumpStep) {
     //Validates all required input tags in the current step
     for (let i = 0; i < requiredElementsForm[stepNumber].length; i++) {
         if (!requiredElementsForm[stepNumber][i].checkValidity()) {
+            //For some reason, some times the function reportValidity() does not display the error correctly if the function is called before the input is visible
+            //This code allows the webpage to first jump into the step containing the error and the show the error in order for the error message to be displayed correctly
+            //This code is of course optional and entirely dependant on the passed variable 'jumpStep'
+            if (jumpStep) {
+                currentStep = stepNumber;
+                setActiveStep();
+            }
             requiredElementsForm[stepNumber][i].reportValidity();
             return false;
         }
@@ -119,27 +175,52 @@ function validateStep(stepNumber) {
 
 function validate(submit) {
     if (submit) {
-        //This part is only executed before submitting the form. It basically validates all steps again.
-        for (let i = 0; i < requiredElementsForm.length; i++) {
+        //The required elements from the registration process  are checked depending on the companyRegistration variable value which determines the path taken (registration with existent company or regitstration of user and company)
+        let nonRequired = null;
+        //Determining the required elements and the validation function depending on the path taken. Some elements might be required (or not) depending on the selected path, therefore its validation also varies depending on it
+        if (companyRegistration) {
+            //In this case, aside from the user info. and the password, the extra data that needs to be validated is the one inside the collapsible 
+            requiredElements = requiredElementsRegisterCompanyForm;
+            nonRequired = requiredElementsForm;
+        } else {
+            //In this case, aside from the user info. and the password, the only extra required data is the company that the user will be registered to
+            requiredElements = requiredElementsForm;
+            nonRequired = requiredElementsRegisterCompanyForm;
+        }
 
-            if (!validateStep(i)) {
-                currentStep = i;
-                setActiveStep();
+        let successful = true;
+        for (let i = 0; i < requiredElements.length; i++) {
+            if (!validateStep(i, requiredElements, true)) {
+                successful = false;
             }
+        }
+
+        //Set all required elements from the other path to non required in order for the browser to not report them. This will be later processed in the server to avoid errors
+        if (successful) {
+            notRequiredElements = nonRequired.flat().filter(x => !requiredElements.flat().includes(x));
+            notRequiredElements.flat().forEach(element => element.required = false);
+            form.submit();
         }
     } else {
         //This part is executed only when the form is loaded. The code looks for the DOM elements with the class 'form-control-danger'
         //which Django places in those form elements that were incorrect when the form was last submitted. If any of these elements
         //is found, then the form jumps to the right step, so that the user knows which step needs to be corrected.
-        let requiredElementsFlat = requiredElementsForm.flat();
-        for (let i = 0; i < requiredElementsFlat.length; i++) {
-            if (requiredElementsFlat[i].classList.contains('form-control-danger')) {
-                if (requiredElementsFlat[i].type == 'password')
+        let formInputElements = form.querySelectorAll('input');
+        for (let i = 0; i < formInputElements.length; i++) {
+            if (formInputElements[i].classList.contains('form-control-danger')) {
+                //Go to the step were the error is displayed. This is selected depending on the id of the element.
+                if (formInputElements[i].id.includes('password'))
                     currentStep = 2;
-                else if (requiredElementsFlat[i].id == 'browser')
-                    currentStep = 0;
-                else
+                else if (formInputElements[i].id.includes('id_contact'))
                     currentStep = 1;
+                else {
+                    currentStep = 0;
+                    //If there's an error inside the collapsible, then we initiallize it open, so its not collapsed by setting the maxHeight to the viewport height
+                    if (formInputElements[i].id != 'browser') {
+                        formInputElements[i].closest(".collapsible-content").style.maxHeight = "100vh";
+                        companyRegistrationForm.classList.add("active");
+                    }
+                }
 
                 break;
             }
@@ -157,7 +238,6 @@ function initBrowser() {
 
         // Make a new timeout set to go off in 1000ms (1 second)
         timeout = setTimeout(function () {
-
 
             //Remove all elements from list
             while (list.firstChild) {
@@ -203,6 +283,7 @@ function initBrowser() {
                             //Trigger confirmation
                             document.getElementById('company-confirmation').style.display = 'block';
                             document.getElementById('company-search').style.display = 'none';
+                            companyRegistrationForm.style.display = 'none';
                             setVisibleButtons(true, true, false, true);
                         });
                     }
@@ -222,8 +303,8 @@ function execute() {
     initBrowser();
 
     nextButton.addEventListener('click', function () {
-        //Limiting the step amount between 0 and numberSteps
-        if (currentStep < numberSteps - 1 && validateStep(currentStep)) {
+        //Limiting the step amount between 0 and numberSteps. Validate the step before jumping to the new one
+        if (currentStep < numberSteps - 1 && validateStep(currentStep, requiredElementsForm, false)) {
             currentStep = currentStep + 1;
             setActiveStep();
         }
@@ -233,6 +314,11 @@ function execute() {
         if (currentStep > 0) {
             //Limiting the step amount between 0 and numberSteps
             currentStep = currentStep - 1;
+
+            //Reset companyRegistration to false, since the user went back to the initial step
+            if (currentStep == 0)
+                companyRegistration = false;
+
         }
         setActiveStep();
     });
@@ -240,15 +326,20 @@ function execute() {
     submitButton.addEventListener('click', function () {
         //Validate all form
         validate(true);
-        if (form.reportValidity()) {
-            //Submit if client-side validation is correct
-            form.submit();
+    });
+
+    registerCompanyButton.addEventListener('click', function () {
+        //Validate all form
+        if (validateStep(currentStep, requiredElementsRegisterCompanyForm, false)) {
+            currentStep = currentStep + 1;
+            //Set the path to 'register user + company'
+            companyRegistration = true;
+            setActiveStep();
         }
     });
 }
 
 (function () {
-
     if (document.readyState !== 'loading') {
         //document is already ready and JS can be executed
         execute();
@@ -258,5 +349,4 @@ function execute() {
             execute();
         });
     }
-
 })();
