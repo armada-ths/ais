f0ece257ffb86ac5e9d6594af466fde727ee085f
Hampus Hallkvist <hampus@hallkvist.org>
2023-08-18 00:43:05 +0200

    fix: Remove unused app.tsx

--- apps/complete_registration/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardScreen } from "./screens/dashboard/screen"
-
-export function App() {
-    return (
-        <div className="bg-slate-50">
-            <DashboardScreen />
-        </div>
-    )
-}
