30abfcb50af422c02f6bf5fe48e243e5b471146a
Didrik Munther <dmu0817@gmail.com>
2023-08-17 17:56:30 +0200

    Add new confirm binding checkbox text

--- apps/complete_registration/src/forms/primary/summary.page.tsx
+++ apps/complete_registration/src/forms/primary/summary.page.tsx
@@ -138,9 +138,7 @@ export function SummaryFormPage() {
                         }
                     />
                     <p>
-                        I understand that the Final Registration is binding and{" "}
-                        <i>{company.companyName}</i> will be invoiced{" "}
-                        <b>{grossPrice} kr</b> (incl. VAT)
+                        I understand that the Final Registration is binding and <i>{company.companyName}</i> will be invoiced <b>{grossPrice} kr</b> inc. VAT, by THS Armada, through Tekniska Högskolans Studentkår, org. nr. 802005-9153
                     </p>
                 </div>
 
