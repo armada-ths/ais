e27249394f7bc274f5fe5248185bb12aa66e21af
e-martinsson <emartinsson@protonmail.com>
2023-08-20 23:20:29 +0200

    changed recruitment-manytomany- to checkbox

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+import django.forms as forms
 
 
 class CustomFieldInline(admin.TabularInline):
@@ -37,8 +38,12 @@ class SlotAdmin(admin.ModelAdmin):
     list_display = ["__str__", "location", "recruitment_period"]
     list_filter = ["location", "recruitment_period"]
 
+@admin.register(RecruitmentPeriod)
+class RecruitmentPeriodModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': forms.CheckboxSelectMultiple},
+    }
 
-admin.site.register(RecruitmentPeriod)
 admin.site.register(RecruitmentApplication, RecruitmentApplicationAdmin)
 admin.site.register(CustomField, CustomFieldAdmin)
 admin.site.register(CustomFieldAnswer)
