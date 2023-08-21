75df9f5ab490fcd7346fa7e6f6710db3b6b638d3
Didrik Munther <dmu0817@gmail.com>
2023-08-21 02:21:23 +0200

    Remove showing old orders in CR cart

--- apps/complete_registration/src/shared/useLoadData.tsx
+++ apps/complete_registration/src/shared/useLoadData.tsx
@@ -27,65 +27,68 @@ export default function useLoadData() {
         initialized.current = true
         setLoading(false)
 
-        fetch(`${HOST}/api/accounting/products`).then(async raw => {
-            const data = await raw.json()
-            console.log("PRODUCS", JSON.stringify(data))
+        const load = async () => {
+            const data = await fetch(`${HOST}/api/accounting/products`).then(raw => raw.json())
+            console.log("PRODUCTS", JSON.stringify(data))
             dispatch(loadProducts(data))
-        })
-        fetch(`${HOST}/api/registration/`, {}).then(async raw => {
-            const data = await raw.json()
-            console.log("DATA", JSON.stringify(data))
 
-            if (data.error != null) {
-                dispatch(setErrors(data.error))
-            }
-
-            const awaitingMappings = reverseMap(data)
-
-            // Set status for company
-            dispatch(
-                setCompanyRegistrationStatus(data.type as RegistrationStatus)
-            )
-            if (data.company?.name) dispatch(setCompanyName(data.company.name))
-            if (data.contact) dispatch(setUser(data.contact))
-            if (data.contract) dispatch(setContract(data.contract))
-
-            for (const current of awaitingMappings) {
+            fetch(`${HOST}/api/registration/`, {}).then(async raw => {
+                const data = await raw.json()
+                console.log("DATA", JSON.stringify(data))
+    
+                if (data.error != null) {
+                    dispatch(setErrors(data.error))
+                }
+    
+                const awaitingMappings = reverseMap(data)
+    
+                // Set status for company
                 dispatch(
-                    setField({
-                        mapping: current.mapping,
-                        value: current.value
-                    })
+                    setCompanyRegistrationStatus(data.type as RegistrationStatus)
                 )
-            }
-
-            // Apply orders
-            const orders = data.orders
-            for (const productMeta of orders ?? []) {
-                dispatch(
-                    pickProduct({
+                if (data.company?.name) dispatch(setCompanyName(data.company.name))
+                if (data.contact) dispatch(setUser(data.contact))
+                if (data.contract) dispatch(setContract(data.contract))
+    
+                for (const current of awaitingMappings) {
+                    dispatch(
+                        setField({
+                            mapping: current.mapping,
+                            value: current.value
+                        })
+                    )
+                }
+    
+                // Apply orders
+                const orders = data.orders
+                for (const productMeta of orders ?? []) {
+                    dispatch(
+                        pickProduct({
+                            id: productMeta.product.id,
+                            quantity: productMeta.quantity,
+                            isPackage:
+                                productMeta.product.category?.name === PACKAGE_KEY
+                        })
+                    )
+                }
+    
+                const productMetas = data.orders
+                const customPrices = [] as Omit<SelectedProduct, "isPackage">[]
+    
+                for (const productMeta of productMetas ?? []) {
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    customPrices.push({
                         id: productMeta.product.id,
+                        comment: productMeta.comment,
                         quantity: productMeta.quantity,
-                        isPackage:
-                            productMeta.product.category?.name === PACKAGE_KEY
+                        adjustedPrice: productMeta.unit_price
                     })
-                )
-            }
+                }
+                dispatch(loadProductMeta(customPrices))
+            })
+        };
 
-            const productMetas = data.orders
-            const customPrices = [] as Omit<SelectedProduct, "isPackage">[]
-
-            for (const productMeta of productMetas ?? []) {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                customPrices.push({
-                    id: productMeta.product.id,
-                    comment: productMeta.comment,
-                    quantity: productMeta.quantity,
-                    adjustedPrice: productMeta.unit_price
-                })
-            }
-            dispatch(loadProductMeta(customPrices))
-        })
+        load()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
