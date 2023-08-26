db85eead4ccf7b7bc702108b2c49bb5bcba8d97b
e-martinsson <emartinsson@protonmail.com>
2023-08-26 15:03:28 +0200

    added autocomplete to FK and m-2-m fields

--- improvedAdmin.py
+++ improvedAdmin.py
@@ -6,9 +6,18 @@ import django.forms as forms
 #Own subclass of ModelAdmin to override appearances of forms on admin panel.
 class ModelAdminImproved(admin.ModelAdmin):
 
-    def __init__(self, model: type, admin_site: AdminSite | None) -> None:
+    def __init__(self, model, admin_site: AdminSite | None):
         super().__init__(model, admin_site)
+        self.search_fields = ['name'] #Default search field
         
+    #Overrides get_autocomplete_fields to be all foreignkey and manytomany fields.
+    def get_autocomplete_fields(self, request):
+        autocomplete_fields = []
+        for field in self.model._meta.fields:
+            if isinstance(field, (models.ForeignKey, models.ManyToManyField)):
+                autocomplete_fields.append(field.name)
+        return autocomplete_fields
+    
     formfield_overrides = {
         models.ManyToManyField: {'widget': forms.CheckboxSelectMultiple()},
     }
