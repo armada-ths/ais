a184a59267d9fdc48f4d97d73abfae3c9f97156b
Hampus Hallkvist <30767818+Hampfh@users.noreply.github.com>
2023-09-03 10:56:06 +0200

    Hampus/fix/transport text (#1023)
    ## Describe your changes
    
    Fixes: #
    
    ## Checklist before requesting review
    
    - [ ] Feature/fix PRs should add one atomic (as small as possible)
    feature or fix.
    - [ ] The code compiles and all the tests pass.

---
+++ apps/complete_registration/src/forms/sture/form.tsx
@@ -0,0 +1,19 @@
+import { Form } from "../form_types"
+import { StureContactPage } from "./sture_contact.page"
+
+export const form: Form = {
+    key: "sture",
+    name: "Content for your booth",
+    description:
+        "Armada are pleased to once again present Sture™ as our fair partner. Sture can help you with all contents for your booth and make it stand out in a great way. They are on the frontier of fair furnishings both when it comes to technologies and sustainable production - a perfect fit for your Armada booth!",
+    progression: "none",
+    pages: [
+        {
+            id: "sture_contact_info",
+            title: "Content for your booth",
+            pageComponent: StureContactPage,
+            hasNextButton: false,
+            fields: []
+        }
+    ]
+}
