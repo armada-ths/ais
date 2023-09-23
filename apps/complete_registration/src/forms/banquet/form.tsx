033e04201e2a161f935ba8b9898ec1f95ca518d1
Ferran Campos <ferran98campos@gmail.com>
2023-09-23 11:00:49 +0200

    Rebase for an easier merge to master
    A rebase has been done + some lines of code (related to the banquet tickets) have been added unded @ts-ignore

--- apps/complete_registration/src/forms/banquet/form.tsx
+++ apps/complete_registration/src/forms/banquet/form.tsx
@@ -2,6 +2,7 @@ import { Form } from "../form_types"
 import { CommingSoon } from "../lunch_tickets/comming_soon.page"
 
 export const form: Form = {
+    //@ts-ignore
     key: "banquet_tickets",
     name: "Banquet Tickets",
     description:
