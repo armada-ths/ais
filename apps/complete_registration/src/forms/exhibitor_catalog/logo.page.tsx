66ce9283630dab353375f38d6a6393e62ae96ad1
Hampus Hallkvist <30767818+Hampfh@users.noreply.github.com>
2023-09-28 18:46:43 +0200

    fix: Show expected file type for logos (#1052)
    ## Describe your changes
    
    Fixes: #
    
    ## Checklist before requesting review
    
    - [ ] Feature/fix PRs should add one atomic (as small as possible)
    feature or fix.
    - [ ] The code compiles and all the tests pass.

--- apps/complete_registration/src/forms/exhibitor_catalog/logo.page.tsx
+++ apps/complete_registration/src/forms/exhibitor_catalog/logo.page.tsx
@@ -4,6 +4,11 @@ import { FormField } from "../../screens/form/FormInput"
 export function LogoFormPage() {
     return (
         <FormWrapper>
+            <div className="rounded bg-orange-100 p-3">
+                <p className="text-orange-500">
+                    File formats must be: (.png, .jpg or .gif)
+                </p>
+            </div>
             <FormField.Image
                 mapping="exhibitor.catalogue_logo_squared"
                 label="Company Logo Square"
