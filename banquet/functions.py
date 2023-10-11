0b2ef0a6ce608d0cb5d7e43eae26c5f30dea9327
Didrik Munther <dmu0817@gmail.com>
2023-10-11 21:16:23 +0200

    Remove commented out code

--- banquet/functions.py
+++ banquet/functions.py
@@ -60,31 +60,3 @@ def send_confirmation_email(request, invitation, name, email_address):
     send_invitation_mail(
         invitation, name, banquet.date, banquet.location, link, email_address
     )
-
-
-# def get_table(table_number):
-#     (table, was_created) = BanquetTable.objects.get_or_create(pk=table_number)
-#     if was_created:
-#         table.table_name = "Table " + str(table_number)
-#         table.number_of_seats = 8
-#         table.save()
-#     return table
-
-
-# def sit_attendants():
-#     """
-#     A helper function that sits down BanquetAttendants at Tables (no optimisation right now)
-#     """
-#     table_number = 1
-#     attendants_at_table = 0
-#     table = get_table(table_number)
-#     for attendant in BanquetteAttendant.objects.all():
-#         if attendant.confirmed:
-#             attendants_at_table += 1
-#             if attendants_at_table > table.number_of_seats:
-#                 table_number += 1
-#                 table = get_table(table_number)
-#                 attendants_at_table = 1
-#             attendant.table = table
-#             attendant.seat_number = attendants_at_table
-#             attendant.save()
