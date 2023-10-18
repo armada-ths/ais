4aad76534bbd17ffc10fbbbc2ba74fa1dbb73b4e
Didrik Munther <dmu0817@gmail.com>
2023-10-18 18:30:37 +0200

    Format

--- events/migrations/0029_add_program_question.py
+++ events/migrations/0029_add_program_question.py
@@ -4,15 +4,23 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('events', '0028_auto_20210918_2247'),
+        ("events", "0028_auto_20210918_2247"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='signupquestion',
-            name='type',
-            field=models.CharField(choices=[('text_field', 'Short Text'), ('text_area', 'Long Text'), ('single_choice', 'Single Choice'), ('multiple_choice', 'Multiple Choice'), ('student_program', 'Student Program')], max_length=20),
+            model_name="signupquestion",
+            name="type",
+            field=models.CharField(
+                choices=[
+                    ("text_field", "Short Text"),
+                    ("text_area", "Long Text"),
+                    ("single_choice", "Single Choice"),
+                    ("multiple_choice", "Multiple Choice"),
+                    ("student_program", "Student Program"),
+                ],
+                max_length=20,
+            ),
         ),
     ]
