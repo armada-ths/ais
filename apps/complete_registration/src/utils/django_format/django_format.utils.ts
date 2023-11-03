7194fa1ecec1b430e4d77ed73bbfa0a0c7f4bfc1
Hampus Hallkvist <hampus@hallkvist.org>
2023-11-03 13:58:37 +0100

    fix: Formatting

--- apps/complete_registration/src/utils/django_format/django_format.utils.ts
+++ apps/complete_registration/src/utils/django_format/django_format.utils.ts
@@ -1,42 +1,44 @@
-export function getCRSFToken () {
-    const name = "csrftoken";
-    const cookies = document.cookie.split(';');
+export function getCRSFToken() {
+    const name = "csrftoken"
+    const cookies = document.cookie.split(";")
     for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.startsWith(name + "=")) {
-        return cookie.substring(name.length + 1, cookie.length);
-      }
+        const cookie = cookies[i].trim()
+        if (cookie.startsWith(name + "=")) {
+            return cookie.substring(name.length + 1, cookie.length)
+        }
     }
-    throw new Error("Cookie not found");
+    throw new Error("Cookie not found")
 }
 
-export function ObjectToURLEncoded(object:object) : string{
-    const mapping = Object.keys(object).map((key) => {
-        const objectKey = key as keyof object;
-        if(!objectKey)
-            return;
-        if(Array.isArray(object[objectKey])){
-            const objectArray : Array<string|number|boolean> = object[objectKey];
-            let result = "";
-            for(let i=0; i<objectArray.length; i++){
-                if(result != "")
-                    result += "&";
-                result += objectKey+"="+objectArray[i];
+export function ObjectToURLEncoded(object: object): string {
+    const mapping = Object.keys(object)
+        .map(key => {
+            const objectKey = key as keyof object
+            if (!objectKey) return
+            if (Array.isArray(object[objectKey])) {
+                const objectArray: Array<string | number | boolean> =
+                    object[objectKey]
+                let result = ""
+                for (let i = 0; i < objectArray.length; i++) {
+                    if (result != "") result += "&"
+                    result += objectKey + "=" + objectArray[i]
+                }
+                return result
             }
-            return result;
-        }
-        if(typeof object[objectKey] === "object")
-            return ObjectToURLEncoded(object[objectKey]);
-        else{
-            return objectKey+"="+object[objectKey];
-        }
-    }).join("&");
+            if (typeof object[objectKey] === "object")
+                return ObjectToURLEncoded(object[objectKey])
+            else {
+                return objectKey + "=" + object[objectKey]
+            }
+        })
+        .join("&")
 
-    return mapping;
+    return mapping
 }
 
-export function buildURLEncodedPayload(data:object){
-    const token = getCRSFToken();
+export function buildURLEncodedPayload(data: object) {
+    const token = getCRSFToken()
     //return ObjectToURLEncoded(data);
-    return "crsfmiddlewaretoken="+token+"&"+ObjectToURLEncoded(data);
+    console.log("test", ObjectToURLEncoded(data))
+    return "crsfmiddlewaretoken=" + token + "&" + ObjectToURLEncoded(data)
 }
