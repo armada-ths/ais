47d9393409987e579b71bf33274cd1173ab3319a
Didrik Munther <5240046+didrikmunther@users.noreply.github.com>
2024-02-23 12:12:54 +0100

    fix: Remove sign out underline (#1160)
    ## Describe your changes
    
    Fixes: #

--- apps/dashboard/src/shared/LogoutButton.tsx
+++ apps/dashboard/src/shared/LogoutButton.tsx
@@ -4,7 +4,7 @@ export function LogoutButton() {
     return (
         <a className="p-3 " href={`${HOST}/accounts/logout?next=/dashboard`}>
             <div className="my-5 flex items-center gap-x-2 text-center text-slate-700 hover:text-slate-500">
-                <p className="underline">Sign Out</p>
+                <p className="hover:underline">Sign Out</p>
                 <span className="pi pi-sign-out" />
             </div>
         </a>
