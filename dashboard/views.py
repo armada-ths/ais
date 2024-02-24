97f0c758bf48c03395621a2ed29ac983dcf4d996
Didrik Munther <dmu0817@gmail.com>
2024-02-24 01:17:27 +0100

    Add dashboard redirect to company id

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,5 @@
-from datetime import timezone
-
 from django.shortcuts import redirect, render
 from companies.models import CompanyContact
-from fair.models import Fair
 
 
 def dashboard_company(request, company_id):
@@ -16,12 +13,14 @@ def dashboard_index(request):
     if not request.user.is_authenticated:
         return redirect("anmalan:choose_company")
 
-    # find all connections between this user and companies
+    # Find all connections between this user and companies
     company_contacts = CompanyContact.objects.filter(user=request.user).exclude(
         company=None
     )
 
     if len(company_contacts) == 1:
-        return render(request, "dashboard/index.html")
+        return redirect(
+            "dashboard:company_dashboard", company_id=company_contacts[0].company.id
+        )
 
     return redirect("anmalan:choose_company")
