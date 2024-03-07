978921735903706e66cb5274b9538b0a9225512d
Hampus Hallkvist <hampus@hallkvist.org>
2024-03-07 23:03:28 +0100

    fix: Debouncer infinity call

--- apps/dashboard/src/shared/hooks/useDebounce.tsx
+++ apps/dashboard/src/shared/hooks/useDebounce.tsx
@@ -4,36 +4,48 @@ export function useDebounce<T>(
     value: T,
     callback: (value: T) => void,
     options?: {
-        delay: number
+        delay?: number
     }
 ) {
+    const initialized = useRef(false)
     const timerRef = useRef<NodeJS.Timeout>()
 
     useEffect(() => {
+        if (!initialized.current) {
+            initialized.current = true
+            return
+        }
         clearTimeout(timerRef.current)
         timerRef.current = setTimeout(
             () => callback(value),
             options?.delay ?? 200
         )
-    }, [callback, options?.delay, value])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value])
 }
 
 export function useDebounceValue<T>(
     value: T,
     options?: {
-        delay: number
+        delay?: number
     }
 ) {
+    const initialized = useRef(false)
     const timerRef = useRef<NodeJS.Timeout>()
     const [debouncedValue, setDebouncedValue] = useState(value)
 
     useEffect(() => {
+        if (!initialized.current) {
+            initialized.current = true
+            return
+        }
         clearTimeout(timerRef.current)
         timerRef.current = setTimeout(
             () => setDebouncedValue(value),
             options?.delay ?? 200
         )
-    }, [options?.delay, value])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value])
 
     return debouncedValue
 }
