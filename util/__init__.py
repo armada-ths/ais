c2f7ce9962066dfc0f2295c323cb2df18393bb2c
Hampus Hallkvist <hampus@hallkvist.org>
2024-03-07 12:16:56 +0100

    fix: Wrong email login in backend

--- util/__init__.py
+++ util/__init__.py
@@ -44,8 +44,7 @@ def get_user(request):
     user = request.user
     if not user.is_authenticated:
         if is_dev:
-            # user = User.objects.filter(email="dashboard@armada.nu").first()
-            user = User.objects.filter(email="hampus+neverseenemail@hallkvi.st").first()
+            user = User.objects.filter(email="dashboard@armada.nu").first()
             if user == None:
                 raise Exception(
                     "No user with email dashboard@armada.nu. Please create one for local development."
