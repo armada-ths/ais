38e040d669c97d3d0ff00210cce3cb582010e578
Hampus Hallkvist <hampus@hallkvist.org>
2024-06-06 11:40:12 +0200

    feat: Add new hook useOrders

--- apps/dashboard/src/shared/hooks/api/useOrders.tsx
+++ apps/dashboard/src/shared/hooks/api/useOrders.tsx
@@ -4,10 +4,10 @@ import { useDashboard } from "@/shared/hooks/api/useDashboard"
  * All available products that can be purchased
  * by the companies
  */
-export function useProducts() {
+export function useOrders() {
     const { data, ...rest } = useDashboard()
     return {
-        data: data?.products ?? [],
+        data: data?.orders ?? [],
         ...rest
     }
 }
