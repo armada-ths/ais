6f57ab682d6ed8eab07331a533cc1794c7538b7c
linus <linusbalt3r@gmail.com>
2024-07-29 15:10:42 +0200

    Merged ir.py and cr.py.

--- dashboard/api/registration/response.py
+++ dashboard/api/registration/response.py
@@ -1,21 +1,22 @@
 import datetime
+from ais.common import settings
+from dashboard.api.registration.types.registration import get_registration
+from exhibitors.models import Exhibitor
+from util import JSONError, get_user, status
 
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from django.conf import settings
 
-from exhibitors.models import Exhibitor
-from util import JSONError, get_user, status
-from util.email import send_mail
-from util.ip import get_client_ip
-
-from dashboard.api.registration.types.registration import get_registration
 from dashboard.api.registration.types.util import get_serializer, put_registration
 
 from register.models import SignupLog
+from accounting.models import Order
+
+from util.email import send_mail
+from util.ip import get_client_ip
 
 
-def handle_ir(request, company, fair, contact, exhibitor):
+def handle_response(request, company, fair, contact, exhibitor):
     try:
         registration = get_registration(company, fair, contact, exhibitor)
     except JSONError as error:
@@ -29,6 +30,71 @@ def handle_ir(request, company, fair, contact, exhibitor):
         return put_registration(request, registration, company)
     else:
         return status.UNSUPPORTED_METHOD
+
+
+@require_POST
+def submit_cr(request, company, fair, contact, exhibitor):
+    try:
+        registration = get_registration(company, fair, contact, exhibitor)
+    except JSONError as error:
+        return error.status
+
+    # Currently will not happen, because you can only
+    # call submit on your own company
+    if registration.contact == None:
+        return status.COMPANY_DOES_NOT_EXIST
+
+    signature = registration.cr_signature
+
+    if signature != None:
+        return status.EXHIBITOR_ALREADY_SIGNED
+
+    signature = SignupLog.objects.create(
+        company_contact=contact,
+        contract=registration.cr_contract,
+        company=company,
+        ip_address=get_client_ip(request),
+    )
+
+    # Add package products
+    package_products = Order.objects.filter(
+        purchasing_company=company,
+        product__revenue__fair=fair,
+        product__category__name="Package",
+    )
+    for package in package_products:
+        for child in package.product.child_products.all():
+            Order.objects.create(
+                purchasing_company=company,
+                product=child.child_product,
+                quantity=child.quantity,
+                unit_price=0,  # A package product is free
+            )
+
+    # Untested
+    # Todo: Add packages to email
+    send_mail(
+        request,
+        template="register/email/cr_complete.html",
+        context={
+            "company": company,
+            "fair": fair,
+            "signature": signature,
+            "deadline": registration.deadline,
+        },
+        subject="Final registration received!",
+        to=[signature.company_contact.email_address],
+        file_paths=[settings.MEDIA_ROOT + signature.contract.contract.url[6:]],
+    )
+
+    try:
+        registration = get_registration(company, fair, contact, exhibitor)
+    except JSONError as error:
+        return error.status
+
+    serializer = get_serializer(request, registration)
+
+    return JsonResponse(serializer.data, safe=False)
 
 
 def send_ir_confirmation_email(
@@ -93,13 +159,13 @@ def submit_ir(request, company, fair, contact):
         company=company,
         ip_address=get_client_ip(request),
     )
-    
+
     exhibitor = Exhibitor.objects.create(fair=fair, company=company)
 
     send_ir_confirmation_email(request, fair, signature, company)
 
     try:
-        registration = get_registration(company, fair, contact, None)
+        registration = get_registration(company, fair, contact, exhibitor)
     except JSONError as error:
         return error.status
 
