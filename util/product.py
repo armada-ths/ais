00c6ae8609b865abe60231ec2c34484efada13ec
Didrik Munther <dmu0817@gmail.com>
2024-08-04 20:17:42 +0200

    Add specific price depending on timely IR signature

--- util/product.py
+++ util/product.py
@@ -6,11 +6,16 @@ from django.db.models import Q
 
 def get_products(fair, company):
     contract, signature = get_contract_signature(company, fair, type="INITIAL")
+    if signature == None:
+        return Product.objects.none()
 
     q = Q(exclusively_for=[])
-    if signature == None:
-        q |= Q(exclusively_for__contains=["ir-unsigned"])
+
+    print("timely", signature.contract.is_timely)
+
+    if signature.contract.is_timely:
+        q |= Q(exclusively_for__contains=["ir-timely"])
     else:
-        q |= Q(exclusively_for__contains=["ir-signed"])
+        q |= Q(exclusively_for__contains=["ir-late"])
 
     return Product.objects.filter(revenue__fair=fair).filter(q)
