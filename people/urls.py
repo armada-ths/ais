582ed67dd03bb0cc34816884d4c43e93704de4d8
Didrik Munther <5240046+didrikmunther@users.noreply.github.com>
2024-09-02 18:07:01 +0200

    fix: Contacts page performance (#1262)
    ## Describe your changes
    
    Fixes: #

--- people/urls.py
+++ people/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "people"
 
 urlpatterns = [
-    url(r"^$", views.list, name="list"),
+    url(r"^$", views.list_people, name="list"),
     url(r"^edit$", views.edit, name="edit"),
     url(r"^(?P<pk>\d+)$", views.profile, name="profile"),
     url(r"^profile_delete/(?P<pk>\d+)$", views.profile_delete, name="profile_delete"),
